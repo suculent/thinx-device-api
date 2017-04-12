cfbaeb4f7239a92cb13af74ef737ce2a31e97614
Matěj Sychra <matej.sychra@t-mobile.cz>
2017-04-12 18:28:38 +0200

    updated deploy script, now working (todo: move implementation into service or re-use)

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 DAEMON="node index.js"
 
-echo "☢  DEPLOYMENT ☢"
+echo
+echo "☢  THiNX RE-DEPLOYMENT STARTED ☢"
 echo
 echo "» Checking environment..."
 
@@ -17,24 +18,25 @@ echo "» Checking if node.js is running..."
 
 NODEZ=$(ps -ax | grep "$DAEMON")
 
-echo "NODEZ:"
-echo "$NODEZ"
-
 echo "${NODEZ}" | while IFS="pts" read A B ; do 
 	NODE=$($A | tr -d ' ')
-	echo $NODE $B
+	echo "Killing: " $NODE $B
 	kill $NODE
 done
 
+echo
 echo "» Fetching current app version from GIT..."
 
 git pull origin master
 
-echo "☢  Running node as a background process..."
+echo
+echo "☢  Running node.js as a background process..."
 
 nohup node index.js > thinx.log &
 
+echo
 echo "» Monitoring log. You can exit any time by pressing ^C and logout. Node.js will be still running."
+echo
 
 tail -f thinx.log
 
