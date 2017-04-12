8e72479b2ce893d62029bbd8ad6ddee68fc01006
Matěj Sychra <matej.sychra@t-mobile.cz>
2017-04-13 00:54:11 +0200

    added empty test spec, needs http request implementation to test actual response according to existing bash tests

---
+++ spec/register_spec.rb
@@ -0,0 +1,9 @@
+# require_relative '../greeter.rb' ?
+
+describe 'Register' do
+  it "should say 'WELCOME' when it receives correct /device/register POST" do
+    # TODO: it might respond with an update
+    greeting = "WELCOME"
+    expect(greeting).to eq "WELCOME"
+  end
+end
