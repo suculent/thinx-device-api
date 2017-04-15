260f09fa725b89643509491c183d1ef800c35663
Matěj Sychra <matej.sychra@t-mobile.cz>
2017-04-15 17:14:28 +0200

    cannot use get (because user is not indexed by owner name in db!)

--- index.js
+++ index.js
@@ -409,7 +409,13 @@ app.post("/device/register", function(req, res) {
 
 	console.log("Serching for owner: " + owner);
 
-	userlib.get(owner, function(err, existing, req, res, api_key) {
+	//Error: Error: missing
+	// Caught exception: TypeError: Cannot read property 'session' of undefined
+
+	userlib.view("users", "owners_by_username", {
+		"key": owner,
+		"include_docs": true // might be useless
+	}, function(err, body, req, res, api_key) {
 
 		if (err) {
 			console.log("Error: " + err.toString());
