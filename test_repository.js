43ae65761efd074ba304b13d0e80512b4606153a
Matěj Sychra <matej.sychra@t-mobile.cz>
2017-04-16 11:02:37 +0200

    watcher fails

--- test_repository.js
+++ test_repository.js
@@ -1,7 +1,22 @@
-var repo = require("./lib/thinx/repository");
+var gulp = require('gulp');
+var repoWatch = require('gulp-repository-watch');
 
-repo.watchRepository("git@@github.com/suculent/thinx-device-api.git");
+gulp.task("repo-watch", function() {
+	repoWatch({
+			repository: "git@github.com/suculent/thinx-device-api.git"
+		})
+		.on("check", function() {
+			console.log("No changes in " + repo_url);
+		})
+		.on("change", function(newHash, oldHash) {
+			console.log(repo_url + "changed from ", oldHash, " to ", newHash);
+		});
+});
+
+//var repo = require("./lib/thinx/repository");
+
+//repo.watchRepository("git@github.com/suculent/thinx-device-api.git");
 
 while (true) {
-	
+
 }
