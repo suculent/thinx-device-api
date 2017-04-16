83eeb21159bd90a8834d03270a6d17193a692b2f
Matěj Sychra <matej.sychra@t-mobile.cz>
2017-04-16 11:50:56 +0200

    working git repository checker (sync, async)

--- test_repository.js
+++ test_repository.js
@@ -3,7 +3,7 @@ var repo = require("./lib/thinx/repository");
 // One-time check
 
 console.log("One-time check:");
-var result1 = repo.checkRepository(".", false);
+var result1 = repo.repositoryChanged(".", false);
 console.log(result1);
 
 console.log("Async watch:");
