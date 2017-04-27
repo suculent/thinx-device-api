2825f19a0944aa129dc280c73626d2d051a1d9b5
Matěj Sychra <matej.sychra@t-mobile.cz>
2017-04-27 23:14:44 +0200

    another bugfix, omg this environment, it’s under bad tests

--- index.js
+++ index.js
@@ -776,8 +776,8 @@ app.delete("/api/user/rsakey/revoke", function(req, res) {
 			for (var i = 0; i < fingerprints.length; i++) {
 				console.log("Parsing finerprint " + fingerprints[i]);
 				var key = doc.rsa_keys[fingerprints[i]];
-				console.log("key: " + key + " compared to " + rsa_key_hash);
-				if (key.indexOf(rsa_key_hash) !== -1) {
+				console.log("key: " + key + " compared to " + rsa_key_fingerprint);
+				if (key.indexOf(rsa_key_fingerprint) !== -1) {
 					delete doc.rsa_keys[fingerprints[i]];
 					delete_key = true;
 					break;
