c994f8e62e46242cbd2a8430774be902281a0f6d
Matěj Sychra <matej.sychra@t-mobile.cz>
2017-04-27 18:52:17 +0200

    fixed request validation

--- index.js
+++ index.js
@@ -523,7 +523,7 @@ app.post("/api/user/rsakey", function(req, res) {
 
 	console.log("/api/user/rsakey");
 
-	if (!validateSecureGETRequest(req)) return;
+	if (!validateSecurePOSTRequest(req)) return;
 
 	if (!validateSession(req, res)) return;
 
