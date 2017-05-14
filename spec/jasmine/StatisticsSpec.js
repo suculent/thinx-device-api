e1e57d43eaeb2a8a3c3b6e1c3f6e729999ce890e
Matěj Sychra <matej.sychra@t-mobile.cz>
2017-05-14 16:55:25 +0200

    fixing build logging

--- spec/jasmine/StatisticsSpec.js
+++ spec/jasmine/StatisticsSpec.js
@@ -1,20 +1,22 @@
 describe("Statistics", function() {
 
-  var Statistics = require('../../lib/thinx/statistics');
+  var s = require('../../lib/thinx/statistics');
 
   beforeEach(function() {
     //stats = new Statistics();
   });
 
-  xit("should be able aggregate today results and store in file", function() {
-    expect(true).toBe(true);
-  });
-
   xit("should be able to return today results for owner", function() {
-    expect(true).toBe(true);
+    var result = s.today(
+      "eaabae0d5165c5db4c46c3cb6f062938802f58d9b88a1b46ed69421809f0bf7f",
+      function(result) {
+        console.log(result);
+        expect(result).toBeDefined();
+      });
   });
 
-  xit("should be able to log", function() {
+  xit("should be able to aggregate statistics", function() {
+    var result = s.aggregate();
     expect(true).toBe(true);
   });
 
