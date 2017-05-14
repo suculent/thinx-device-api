5cf8235f6a84d1f731f17eae62f0ade274b27405
Matěj Sychra <matej.sychra@t-mobile.cz>
2017-05-14 17:13:26 +0200

    working on statistics testing

--- spec/jasmine/StatisticsSpec.js
+++ spec/jasmine/StatisticsSpec.js
@@ -2,11 +2,7 @@ describe("Statistics", function() {
 
   var s = require('../../lib/thinx/statistics');
 
-  beforeEach(function() {
-    //stats = new Statistics();
-  });
-
-  xit("should be able to return today results for owner", function() {
+  it("should be able to return today results for owner", function() {
     var result = s.today(
       "eaabae0d5165c5db4c46c3cb6f062938802f58d9b88a1b46ed69421809f0bf7f",
       function(result) {
@@ -15,7 +11,7 @@ describe("Statistics", function() {
       });
   });
 
-  xit("should be able to aggregate statistics", function() {
+  it("should be able to aggregate statistics", function() {
     var result = s.aggregate();
     expect(true).toBe(true);
   });
