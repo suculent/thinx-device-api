87a99e14136f7932bf9df233b5a000ade0173fb8
Matěj Sychra <matej.sychra@t-mobile.cz>
2017-05-19 17:04:36 +0200

    improved api key tests (verify)

--- spec/jasmine/ApikeySpec.js
+++ spec/jasmine/ApikeySpec.js
@@ -5,75 +5,72 @@ describe("API Key", function() {
 
   //create: function(owner, apikey_alias, callback)
   it("should be able to generate new API Keys", function() {
-    //var APIKey = require("../../lib/thinx/apikey");
-    var object = APIKey.create(
+    APIKey.create(
       "eaabae0d5165c5db4c46c3cb6f062938802f58d9b88a1b46ed69421809f0bf7f",
       "sample-key",
       function(success,
         object) {
         if (success) {
-          console.log("Created: " + JSON.stringify(object));
+          console.log("[jasmine] Created: " + JSON.stringify(object));
           generated_key_hash = object.hash;
         } else {
-          console.log("Failed.");
+          console.log("[jasmine] Failed.");
         }
         expect(object).toBeDefined();
-        //done();
+        done();
       });
 
   }, 1000);
 
   //verify: function(owner, apikey, callback)
   it("should be able to verify API Keys", function() {
-    //var APIKey = require('../../lib/thinx/apikey');
-    var object = APIKey.verify(
+    APIKey.verify(
       "eaabae0d5165c5db4c46c3cb6f062938802f58d9b88a1b46ed69421809f0bf7f",
-      "d4d37cb2c00766f433a5feb7e3b97d82fb4b8971",
+      "310c20ed9dcd4663551bd04cc53a58a52c8d9f1b",
       function(success,
         object) {
         if (success) {
-          console.log("Verified: " + JSON.stringify(object));
+          console.log("[jasmine] Verified: " + JSON.stringify(object));
         } else {
           console.log("Verification failed.");
         }
-        expect(object).toBeDefined();
-        //done();
+        expect(success).toBe(true);
+        done();
       });
 
   }, 1000);
 
   //revoke: function(owner, apikey_hash, callback)
   it("should be able to revoke API Keys", function() {
-    //var APIKey = require('../../lib/thinx/apikey');
-    var object = APIKey.revoke(
+    APIKey.revoke(
       "d4d37cb2c00766f433a5feb7e3b97d82fb4b8971",
       "sample-key-hash",
       function(success,
         object) {
         if (success) {
-          console.log("Revoked: " + JSON.stringify(object));
+          console.log("[jasmine] Revoked: " + JSON.stringify(object));
         } else {
-          console.log("Revocation failed.");
+          console.log("[jasmine] Revocation failed.");
         }
         expect(object).toBeDefined();
-        //done();
+        done();
       });
 
   }, 1000);
 
   //list: function(owner, callback)
   it("should be able to list API Keys", function() {
-    var object = APIKey.list(
+    APIKey.list(
       "eaabae0d5165c5db4c46c3cb6f062938802f58d9b88a1b46ed69421809f0bf7f",
       function(success,
         object) {
         if (success) {
-          console.log("Listed: " + JSON.stringify(object));
+          console.log("[jasmine] Listed: " + JSON.stringify(object));
         } else {
-          console.log("Listing failed.");
+          console.log("[jasmine] Listing failed.");
         }
         expect(object).toBeDefined();
-        //done();
+        done();
       });
 
   }, 1000);
