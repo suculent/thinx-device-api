71319e13d51268d8e7d549a9bdd4dfb4d386b427
Matěj Sychra <matej.sychra@t-mobile.cz>
2017-05-20 18:33:12 +0200

    THX-54: statistics aggregator fixed and working under some basic tests (no jasmine so far)

---
+++ stats_test.js
@@ -0,0 +1,18 @@
+var stats = require("./lib/thinx/statistics");
+
+var owner = "eaabae0d5165c5db4c46c3cb6f062938802f58d9b88a1b46ed69421809f0bf7f";
+
+stats.today(owner, function(success, body) {
+
+  if (success) {
+    console.log("[TEST] success:", success);
+    return;
+  }
+
+  if (!body) {
+    console.log("[TEST] Statistics for owner " + owner + " not found.");
+    return;
+  }
+
+  console.log("[TEST] result of stats.today callback: " + body);
+});
