82fc422784d91d946c25138cb85880d52f924e59
Matěj Sychra <matej.sychra@t-mobile.cz>
2017-05-21 21:05:14 +0200

    fixing ws integration

--- index.js
+++ index.js
@@ -3482,7 +3482,9 @@ var ThinxApp = function() {
   // FIXME: Link to letsencrypt SSL keys using configuration for CircleCI
   https.createServer(options, app).listen(serverPort + 1);
   http.createServer(app).listen(serverPort);
-  var wserver = http.createServer(app).listen(7447);
+
+  ver wsapp = express();
+  var wserver = http.createServer(wsapp);
 
   /*
    * WebSocket Server
