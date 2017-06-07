d4a282c7c94bd16f237032af7f0325f8fe3e69f7
Matěj Sychra <matej.sychra@t-mobile.cz>
2017-06-08 00:54:32 +0200

    minor mostly non-function fixes, disabled potentialy dangerous test

--- spec/jasmine/RsakeySpec.js
+++ spec/jasmine/RsakeySpec.js
@@ -6,7 +6,7 @@ describe("RSA Key", function() {
   xit("should be able to add RSA Keys first", function() {});
 
   //revoke: function(owner, revoked_fingerprints, callback)
-  it("should be able to revoke multiple RSA Keys at once", function() {
+  xit("should be able to revoke multiple RSA Keys at once", function() {
     var owner =
       "eaabae0d5165c5db4c46c3cb6f062938802f58d9b88a1b46ed69421809f0bf7f";
     var revoked_fingerprints = [
