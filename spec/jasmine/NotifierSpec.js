9798ec03d1ceb7d036b833340a2fb1bfebad5243
Matěj Sychra <matej.sychra@t-mobile.cz>
2017-06-12 12:27:19 +0200

    various test-driven fixes (minors to make tests fail without side-effects)

--- spec/jasmine/NotifierSpec.js
+++ spec/jasmine/NotifierSpec.js
@@ -13,19 +13,19 @@ describe("Notifier", function() {
     // Calling notifier is a mandatory on successful builds, as it creates the JSON build envelope
     // (or stores into DB later)
 
-    // Hey, this should be JUST a notification, tyfole, no destructives.
-    var test_build_id = Date().toString();
+    // Hey, this should be JUST a notification, no destructives.
+    var test_build_id = "no_build_id";
     var test_commit_id = "crime_commit_id";
     var test_version = "v0.0";
     var test_repo =
       "https://github.com/suculent/thinx-firmware-esp8266.git";
     var test_binary = "nothing.bin";
     var test_udid = "00:00:00:00:00:00";
-    var sha = "who knows";
+    var sha = "one-sha-256-pls";
     var owner_id = owner;
-    var status = "TESTED_SO_FAR";
+    var status = "TESTING_NOTIFIER";
 
-    var CMD =
+    var CMD = "node notifier.js " +
       test_build_id + " " +
       test_commit_id + " " +
       test_version + " " +
@@ -36,14 +36,10 @@ describe("Notifier", function() {
       owner_id + " " +
       status;
 
-
     // CMD: "${BUILD_ID} ${COMMIT} ${VERSION} ${GIT_REPO} ${DEPLOYMENT_PATH}/${BUILD_ID}.bin ${UDID} ${SHA} ${OWNER_ID} ${STATUS}";
-
     var exec = require("child_process");
     var temp = exec.execSync(CMD);
-
     expect(temp).toBeDefined();
-    //done();
 
   }, 10000);
 
