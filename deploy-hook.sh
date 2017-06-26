2f74887418b45d52e069ddf1570ef22038b5e04b
Matěj Sychra <suculent@me.com>
2017-06-26 15:44:26 +0200

    removed deprecated files

--- deploy-hook.sh
+++ deploy-hook.sh
@@ -2,5 +2,5 @@
 
 # There is a commit-hook configured on GitHub. Repository gets pulled and app restarted on commit to 'master'
 
-nohup githooked -p 9001 bash "git pull origin master; pm2 restart all" &
+nohup githooked -p 9001 bash "cd /root/thinx-device-api; git pull origin master; pm2 pullAndRestart" &
 
