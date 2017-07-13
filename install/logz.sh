4820571136c510fbe2848ea9e7ae28190ff22c47
production <root@thinx.cloud>
2017-07-13 10:27:44 +0000

    logz.io analytics integration

---
+++ install/logz.sh
@@ -0,0 +1,18 @@
+wget https://raw.githubusercontent.com/logzio/public-certificates/master/COMODORSADomainValidationSecureServerCA.crt
+sudo mkdir -p /etc/pki/tls/certs
+sudo cp COMODORSADomainValidationSecureServerCA.crt /etc/pki/tls/certs/
+
+#curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.5.0-amd64.deb
+#sudo dpkg -i filebeat-5.5.0-amd64.deb
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+sudo apt-get update && sudo apt-get install filebeat
+
+
+mkdir -p /etc/filebeat
+cp ./filebeat.yml /etc/filebeat/filebeat.yml
+
+
+
