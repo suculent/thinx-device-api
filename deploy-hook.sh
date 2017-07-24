16fedcfa23aa3139ffa80156c4368c16e42d9ae2
Matěj Sychra <suculent@me.com>
2017-07-24 22:34:23 +0200

    disabled colliding deploy hook

--- deploy-hook.sh
+++ deploy-hook.sh
@@ -2,5 +2,6 @@
 
 # There is a commit-hook configured on GitHub. Repository gets pulled and app restarted on commit to 'master'
 
-nohup githooked -p 9001 bash "cd /root/thinx-device-api; git pull origin master; pm2 pullAndRestart" &
+echo "Deploy hook disabled, collides with THiNX itself!"
 
+# nohup githooked -p 9001 bash "cd /root/thinx-device-api; git pull origin master; pm2 pullAndRestart" &
