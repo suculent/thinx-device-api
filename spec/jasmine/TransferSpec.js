2ffaeb93bb1bfe5b1b36eb60599b7eee2497ac3c
Matěj Sychra <suculent@me.com>
2017-08-14 21:11:13 +0200

    transfer async switch

--- spec/jasmine/TransferSpec.js
+++ spec/jasmine/TransferSpec.js
@@ -21,41 +21,39 @@ describe("Transfer", function() {
       expect(success).toBe(true);
       expect(response).toBeDefined();
       this.dynamic_transfer_request_id = response;
-
-      //decline: function(body, callback) {
-      it("should be able to decline device transfer", function(done) {
-        var body = {
-          transfer_id: this.dynamic_transfer_request_id,
-          udids: [envi.udid]
-        };
-        Transfer.decline(body, function(success, response) {
-          expect(success).toBe(true);
-          expect(response).toBeDefined();
-          console.log("transfer decline response: " + JSON.stringify(
-            response));
-          done();
-        });
-      }, 5000);
-
-      //accept: function(body, callback) {
-      it("should be able to accept transferred devices", function(
-        done) {
-        var body = {
-          transfer_id: this.dynamic_transfer_request_id,
-          udids: [envi.udid]
-        };
-        Transfer.accept(body, function(success, response) {
-          expect(success).toBe(true);
-          expect(response).toBeDefined();
-          console.log("transfer accept response: " + JSON.stringify(
-            response));
-          done();
-        });
-      }, 5000);
-
       done();
     });
-
   }, 10000);
+
+  //decline: function(body, callback) {
+  it("should be able to decline device transfer", function(done) {
+    var body = {
+      transfer_id: this.dynamic_transfer_request_id,
+      udids: [envi.udid]
+    };
+    Transfer.decline(body, function(success, response) {
+      expect(success).toBe(true);
+      expect(response).toBeDefined();
+      console.log("transfer decline response: " + JSON.stringify(
+        response));
+      done();
+    });
+  }, 5000);
+
+  //accept: function(body, callback) {
+  it("should be able to accept transferred devices", function(
+    done) {
+    var body = {
+      transfer_id: this.dynamic_transfer_request_id,
+      udids: [envi.udid]
+    };
+    Transfer.accept(body, function(success, response) {
+      expect(success).toBe(true);
+      expect(response).toBeDefined();
+      console.log("transfer accept response: " + JSON.stringify(
+        response));
+      done();
+    });
+  }, 5000);
 
 });
