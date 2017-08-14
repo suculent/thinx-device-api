d4bf6856dd6fb8a32b2abf7549100a9115ce5cd3
Matěj Sychra <suculent@me.com>
2017-08-14 21:33:19 +0200

    TransferSpec causes tests to crash, why?

--- spec/jasmine/TransferSpec.js
+++ spec/jasmine/TransferSpec.js
@@ -10,7 +10,8 @@ describe("Transfer", function() {
   // request: function(owner, body, callback) {
   // body should look like { "to":"some@email.com", "udids" : [ "some-udid", "another-udid" ] }
 
-  it("should be able to initiate device transfer", function(done) {
+  it("should be able to initiate device transfer for decline", function(
+    done) {
     var body = {
       to: "cimrman@thinx.cloud",
       udids: [envi.udid]
@@ -21,39 +22,55 @@ describe("Transfer", function() {
       expect(success).toBe(true);
       expect(response).toBeDefined();
       this.dynamic_transfer_request_id = response;
+
+      //decline: function(body, callback) {
+      it("should be able to decline device transfer", function(done) {
+        var body = {
+          transfer_id: this.dynamic_transfer_request_id,
+          udids: [envi.udid]
+        };
+        Transfer.decline(body, function(success, response) {
+          expect(success).toBe(true);
+          expect(response).toBeDefined();
+          console.log("transfer decline response: " + JSON.stringify(
+            response));
+          done();
+        });
+      }, 5000);
+
       done();
     });
   }, 10000);
 
-  //decline: function(body, callback) {
-  it("should be able to decline device transfer", function(done) {
+  it("should be able to initiate device transfer for accept", function(done) {
     var body = {
-      transfer_id: this.dynamic_transfer_request_id,
+      to: "cimrman@thinx.cloud",
       udids: [envi.udid]
     };
-    Transfer.decline(body, function(success, response) {
+
+    Transfer.request(owner, body, function(success, response) {
+      console.log(response);
       expect(success).toBe(true);
       expect(response).toBeDefined();
-      console.log("transfer decline response: " + JSON.stringify(
-        response));
+      this.dynamic_transfer_request_id = response;
+
+      //accept: function(body, callback) {
+      it("should be able to accept transferred devices", function(
+        done) {
+        var body = {
+          transfer_id: this.dynamic_transfer_request_id,
+          udids: [envi.udid]
+        };
+        Transfer.accept(body, function(success, response) {
+          expect(success).toBe(true);
+          expect(response).toBeDefined();
+          console.log("transfer accept response: " + JSON.stringify(
+            response));
+          done();
+        });
+      }, 5000);
+
       done();
     });
-  }, 5000);
-
-  //accept: function(body, callback) {
-  it("should be able to accept transferred devices", function(
-    done) {
-    var body = {
-      transfer_id: this.dynamic_transfer_request_id,
-      udids: [envi.udid]
-    };
-    Transfer.accept(body, function(success, response) {
-      expect(success).toBe(true);
-      expect(response).toBeDefined();
-      console.log("transfer accept response: " + JSON.stringify(
-        response));
-      done();
-    });
-  }, 5000);
-
+  }, 10000);
 });
