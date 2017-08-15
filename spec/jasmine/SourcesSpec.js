cb2f74517cef52699c86954ef45a44068ee4c387
Matěj Sychra <suculent@me.com>
2017-08-15 11:59:56 +0200

    fixed test repo path and flattened async test hierarchy

--- spec/jasmine/SourcesSpec.js
+++ spec/jasmine/SourcesSpec.js
@@ -8,15 +8,6 @@ describe("Owner", function() {
 
   var source_id = null;
 
-  it("should be able to list owner sources", function(done) {
-    Sources.list(owner, function(success, response) {
-      expect(success).toBe(true);
-      expect(response).toBeDefined();
-      console.log(response);
-      done();
-    });
-  }, 10000);
-
   it("should be able to add source", function(done) {
     Sources.add(owner, "test-git-repo",
       "https://github.com/suculent/thinx-device-api",
@@ -26,21 +17,29 @@ describe("Owner", function() {
         expect(response).toBeDefined();
         source_id = response.source_id;
         console.log(response);
-
-        it("should be able to remove previously added source",
-          function(done) {
-            Sources.remove(owner, [source_id], function(success,
-              response) {
-              expect(success).toBe(true);
-              expect(response).toBeDefined();
-              source_id = response.source_id;
-              console.log(response);
-              done();
-            });
-          }, 10000);
-
         done();
       });
   }, 10000);
+
+  it("should be able to list owner sources", function(done) {
+    Sources.list(owner, function(success, response) {
+      expect(success).toBe(true);
+      expect(response).toBeDefined();
+      console.log(response);
+      done();
+    });
+  }, 10000);
+
+  it("should be able to remove previously added source",
+    function(done) {
+      Sources.remove(owner, [source_id], function(success,
+        response) {
+        expect(success).toBe(true);
+        expect(response).toBeDefined();
+        source_id = response.source_id;
+        console.log(response);
+        done();
+      });
+    }, 10000);
 
 });
