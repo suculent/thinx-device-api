1da6f05f4953ef62703e16c85ad2159c9ec1a363
Matěj Sychra <suculent@me.com>
2017-08-18 17:15:44 +0200

    chasing undefined mac, added test for normalization

--- spec/jasmine/DeviceSpec.js
+++ spec/jasmine/DeviceSpec.js
@@ -45,29 +45,29 @@ describe("Device", function() {
 
   var body = JRS; // JSON.parse(RS);
 
-  console.log("Using test API_KEY: " + apikey);
-  console.log("Using request: " + JSON.stringify(JRS));
+  console.log("• DeviceSpec.js: Using test API_KEY: " + apikey);
+  console.log("• DeviceSpec.js: Using request: " + JSON.stringify(JRS));
 
   it("should be able to register itself.", function(done) {
 
     device.register(JRS, apikey,
       function(success, response) {
-        console.log("Registration result: " + JSON.stringify(response));
+        console.log("• DeviceSpec.js: Registration result: " + JSON.stringify(response));
         expect(success).toBe(true);
         expect(this.udid).toBeDefined();
         this.udid = response.udid;
-        console.log("Received UDID: " + this.udid);
+        console.log("• DeviceSpec.js: Received UDID: " + this.udid);
         done();
 
         it("should be able to provide device firmware",
           function(done) {
             // Returns "OK" when current firmware is valid.
             body.udid = this.udid;
-            console.log("Using this.UDID: " + this.udid);
+            console.log("• DeviceSpec.js: Using this.UDID: " + this.udid);
             device.firmware(body, apikey, function(
               success,
               response) {
-              console.log("Firmware fetch result: " +
+              console.log("• DeviceSpec.js: Firmware fetch result: " +
                 JSON.stringify(
                   response));
               expect(success).toBe(false);
@@ -87,7 +87,7 @@ describe("Device", function() {
       };
       device.edit(owner, changes, function(
         success, response) {
-        console.log("Editing result: " + JSON
+        console.log("• DeviceSpec.js: Editing result: " + JSON
           .stringify(response));
         expect(success).toBe(true);
         expect(response).toBeDefined();
@@ -100,7 +100,7 @@ describe("Device", function() {
     function(done) {
       device.register(JRS2, apikey,
         function(success, response) {
-          console.log("Re-registration result: " + JSON.stringify(
+          console.log("• DeviceSpec.js: Re-registration result: " + JSON.stringify(
             response));
           expect(success).toBe(true);
           done();
@@ -114,18 +114,23 @@ describe("Device", function() {
       expect(response.ott).toBeDefined();
       this.ott = response.ott;
       done();
-
-      it("should be able to fetch OTT request",
-        function(done) {
-          expect(this.ott).toBeDefined();
-          device.fetchOTT(this.ott, function(success,
-            response) {
-            expect(success).toBe(true);
-            expect(response).toBeDefined();
-            done();
-          });
-        }, 5000);
-
     });
   }, 5000);
+
+  it("should be able to fetch OTT request",
+    function(done) {
+      expect(this.ott).toBeDefined();
+      device.fetchOTT(this.ott, function(success,
+        response) {
+        expect(success).toBe(true);
+        expect(response).toBeDefined();
+        done();
+      });
+    }, 5000);
+
+  it("should be able to normalize a MAC address", function(done) {
+    var nmac = device.normalizedMAC("123456789012");
+    expect(nmac).toBeDefined();
+  });
+
 });
