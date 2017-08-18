68fa6dfdebdcdb597b37968d657b7fc16d4a7b79
Matěj Sychra <suculent@me.com>
2017-08-18 12:23:27 +0200

    builderspec must be naked

--- spec/jasmine/BuilderSpec.js
+++ spec/jasmine/BuilderSpec.js
@@ -1,16 +1,14 @@
 describe("Builder", function() {
 
-  beforeEach(function() {
-    //anything in here will apply to everything in each nested describe
-    var builder = require("../../lib/thinx/builder");
+  //anything in here will apply to everything in each nested describe
+  var builder = require("../../lib/thinx/builder");
 
-    var envi = require("./_envi.json");
-    var owner = envi.oid;
-    var udid = envi.udid;
-    var apikey = envi.ak;
-    var build_id = envi.build_id; // "f168def0-597f-11e7-a932-014d5b00c004";
-    var source_id = envi.sid;
-  });
+  var envi = require("./_envi.json");
+  var owner = envi.oid;
+  var udid = envi.udid;
+  var apikey = envi.ak;
+  var build_id = envi.build_id; // "f168def0-597f-11e7-a932-014d5b00c004";
+  var source_id = envi.sid;
 
   it("should be able to initialize", function() {
     expect(builder).toBeDefined();
