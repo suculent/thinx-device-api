9bda754bdfc6b4aaac32829080b2c1717c410b1d
Matěj Sychra <suculent@me.com>
2017-12-06 11:36:31 +0100

    completed migrating to couchdb 2

--- couch_cleanup.sh
+++ couch_cleanup.sh
@@ -28,34 +28,34 @@ curl -XPOST ${PREFIX}_replicate -H 'Content-Type: application/json' -d'{"source"
 echo "Migration of builds completed."
 
 # swap databases
-rm /var/lib/couchdb/managed_builds.backup
-mv /var/lib/couchdb/managed_builds.couch /var/lib/couchdb/managed_builds.backup
-mv /var/lib/couchdb/managed_builds_${DATE}.couch /var/lib/couchdb/managed_builds.couch
+rm /opt/couchdb/data/managed_builds.backup
+mv /opt/couchdb/data/managed_builds.couch /opt/couchdb/data/managed_builds.backup
+mv /opt/couchdb/data/managed_builds_${DATE}.couch /opt/couchdb/data/managed_builds.couch
 
 curl -XPOST ${PREFIX}_replicate -H 'Content-Type: application/json' -d'{"source":"managed_devices","target":"managed_devices_'${DATE}'", "create_target":true }'
 echo "Migration of devices completed."
 
-rm /var/lib/couchdb/managed_devices.backup
-mv /var/lib/couchdb/managed_devices.couch /var/lib/couchdb/managed_devices.backup
-mv /var/lib/couchdb/managed_devices_${DATE}.couch /var/lib/couchdb/managed_devices.couch
+rm /opt/couchdb/data/managed_devices.backup
+mv /opt/couchdb/data/managed_devices.couch /opt/couchdb/data/managed_devices.backup
+mv /opt/couchdb/data/managed_devices_${DATE}.couch /opt/couchdb/data/managed_devices.couch
 
 curl -XPOST ${PREFIX}_replicate -H 'Content-Type: application/json' -d'{"source":"managed_logs","target":"managed_logs_'${DATE}'", "create_target":true }'
 echo "Migration of logs completed."
 
-rm /var/lib/couchdb/managed_logs.backup
-mv /var/lib/couchdb/managed_logs.couch /var/lib/couchdb/managed_logs.backup
-mv /var/lib/couchdb/managed_logs_${DATE}.couch /var/lib/couchdb/managed_logs.couch
+rm /opt/couchdb/data/managed_logs.backup
+mv /opt/couchdb/data/managed_logs.couch /opt/couchdb/data/managed_logs.backup
+mv /opt/couchdb/data/managed_logs_${DATE}.couch /opt/couchdb/data/managed_logs.couch
 
 curl -XPOST ${PREFIX}_replicate -H 'Content-Type: application/json' -d'{"source":"managed_sources","target":"managed_sources_'${DATE}'", "create_target":true }'
 echo "Migration of sources completed."
 
-rm /var/lib/couchdb/managed_sources.backup
-mv /var/lib/couchdb/managed_sources.couch /var/lib/couchdb/managed_sources.backup
-mv /var/lib/couchdb/managed_sources_${DATE}.couch /var/lib/couchdb/managed_sources.couch
+rm /opt/couchdb/data/managed_sources.backup
+mv /opt/couchdb/data/managed_sources.couch /opt/couchdb/data/managed_sources.backup
+mv /opt/couchdb/data/managed_sources_${DATE}.couch /opt/couchdb/data/managed_sources.couch
 
 curl -XPOST ${PREFIX}_replicate -H 'Content-Type: application/json' -d'{"source":"managed_users","target":"managed_users_'${DATE}'", "create_target":true, "filter":"users/del"}'
 echo "Migration of users (without deletions) completed."
 
-rm /var/lib/couchdb/managed_users.backup
-mv /var/lib/couchdb/managed_users.couch /var/lib/couchdb/managed_users.backup
-mv /var/lib/couchdb/managed_users_${DATE}.couch /var/lib/couchdb/managed_users.couch
+rm /opt/couchdb/data/managed_users.backup
+mv /opt/couchdb/data/managed_users.couch /opt/couchdb/data/managed_users.backup
+mv /opt/couchdb/data/managed_users_${DATE}.couch /opt/couchdb/data/managed_users.couch
