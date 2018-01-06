5fd4d6d5011024aefc8d72f37606e2ca1aa44c25
Matěj Sychra <suculent@me.com>
2018-01-06 12:38:39 +0100

    reordered audit test to create a record first

--- spec/jasmine/AuditSpec.js
+++ spec/jasmine/AuditSpec.js
@@ -3,7 +3,15 @@ var owner = envi.oid;
 
 describe("Audit log", function() {
 
-  var Audit = require('../../lib/thinx/audit');
+  var Audit = require('../../lib/thinx/audit');  
+
+  it("should be able to log", function(done) {
+    var s = Audit.log(
+      owner,
+      "Log test successful.");
+    expect(true).toBe(true);
+    done();
+  });
 
   it("should be able to fetch audit log", function(done) {
     var r = Audit.fetch(
@@ -14,14 +22,6 @@ describe("Audit log", function() {
         done();
       }
     );
-  });
-
-  it("should be able to log", function(done) {
-    var s = Audit.log(
-      owner,
-      "Log test successful.");
-    expect(true).toBe(true);
-    done();
-  });
+  }, 15000);
 
 });
