55b2a49153505d0a583fe0428d7a64453ffcfd11
Matěj Sychra <suculent@me.com>
2018-04-13 11:15:48 +0200

    semantic and security fixes

--- index.js
+++ index.js
@@ -3336,7 +3336,7 @@ var ThinxApp = function() {
   }
 
   // Kill existing transformers if any
-  console.log("Docker ST check result: " + result);
+  console.log("Docker ST check result: " + container_already_running);
   if (container_already_running) {
     try {
       exec.execSync("docker kill "+result);
