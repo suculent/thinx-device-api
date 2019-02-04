a0c01392747cb432e12a88284a081a12dba215c3
Matěj Sychra <syxra@syxra.cz>
2019-02-04 11:32:05 +0100

    adding Sonarcloud integration

---
+++ 99-sonar.sh
@@ -0,0 +1,8 @@
+
+
+sonar-scanner \
+  -Dsonar.projectKey=suculent_thinx-device-api \
+  -Dsonar.organization=suculent-github \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=$SONARCLOUD_KEY_THX
