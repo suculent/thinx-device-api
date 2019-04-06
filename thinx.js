e16f12c00dfc4097969d737d7507d9edb122e228
igraczech <matej@keyguru.eu>
2019-04-06 10:36:11 +0200

    responds with buffer only on success

--- thinx.js
+++ thinx.js
@@ -1098,11 +1098,15 @@ var ThinxApp = function() {
     //
     device.ott_update(ott, function(success, response) {
       console.log("Should respond with contents: " + JSON.stringify(response));
-      res.setHeader('Content-Type', 'application/octet-stream');
-      res.setHeader('Content-Disposition', 'attachment; filename=firmware.bin');
-      res.setHeader('Content-Length', response.filesize);
-      res.setHeader('x-MD5', response.md5);
-      respond(res, response.buffer);
+      if (success) {
+        res.setHeader('Content-Type', 'application/octet-stream');
+        res.setHeader('Content-Disposition', 'attachment; filename=firmware.bin');
+        res.setHeader('Content-Length', response.filesize);
+        res.setHeader('x-MD5', response.md5);
+        respond(res, response.buffer);
+      } else {
+        respond(res, response);
+      }
     });
   });
 
