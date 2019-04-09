a278c891f2a9da63a97448ff21887a81a29532cf
production <root@thinx.cloud>
2019-04-09 12:28:19 +0000

    deploy hook update

--- deploy-hook.sh
+++ deploy-hook.sh
@@ -4,5 +4,5 @@
 
 # npm install removed as it takes too long...
 
-nohup githooked -p 9000 bash "cd /root/thinx-device-api; git pull origin master --recurse-submodules; bash ./tools/install-builders.sh; snyk protect; curl https://api.rollbar.com/api/1/deploy/ -F access_token=6aa9f20bef804b75a50338e03830919d -F environment=production -F revision=$(git log -n 1 --pretty=format:\"%H\") -F local_username=$(whoami); pm2 restart ecosystem.json" &
+nohup githooked -p 9000 bash "cd ~/thinx-device-api; git pull origin master --recurse-submodules; bash ./tools/install-builders.sh; snyk protect; curl https://api.rollbar.com/api/1/deploy/ -F access_token=6aa9f20bef804b75a50338e03830919d -F environment=production -F revision=$(git log -n 1 --pretty=format:\"%H\") -F local_username=$(whoami); pm2 restart ecosystem.json" &
 nohup githooked -p 9009 bash "cd /var/www/rtm/www; git pull origin current" &
