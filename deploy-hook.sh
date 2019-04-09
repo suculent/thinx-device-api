d0546c952e43aaab7fb2656066ba6a26a8c4ae5e
igraczech <suculent@me.com>
2019-04-09 14:15:38 +0200

    added shellescape in operations that involve user input and shell (GIT checkouts); fixing registration error (timezone is not defined)

--- deploy-hook.sh
+++ deploy-hook.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # There is a commit-hook configured on GitHub. Repository gets pulled and app restarted on commit to 'master'
+# npm install can be temporarily removed as it takes too long...
 
-# npm install removed as it takes too long...
-
-nohup githooked -p 9000 bash "cd /root/thinx-device-api; git pull origin master --recurse-submodules; bash ./tools/install-builders.sh; snyk protect; curl https://api.rollbar.com/api/1/deploy/ -F access_token=6aa9f20bef804b75a50338e03830919d -F environment=production -F revision=$(git log -n 1 --pretty=format:\"%H\") -F local_username=$(whoami); pm2 restart ecosystem.json" &
-nohup githooked -p 9009 bash "cd /var/www/rtm/www; git pull origin current" &
+nohup githooked -p 9000 bash "cd /var/www/rtm/www; git pull origin current && cd /root/thinx-device-api; git pull origin master --recurse-submodules; bash ./tools/install-builders.sh; npm install .; snyk protect; curl https://api.rollbar.com/api/1/deploy/ -F access_token=6aa9f20bef804b75a50338e03830919d -F environment=production -F revision=$(git log -n 1 --pretty=format:\"%H\") -F local_username=$(whoami); pm2 restart ecosystem.json" &
