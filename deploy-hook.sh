c653150dee0c02811fc37626ac5b85fcdb5a4ca3
igraczech <suculent@me.com>
2019-05-09 11:17:17 +0200

    limiting webhook, resolving shellescape errors

--- deploy-hook.sh
+++ deploy-hook.sh
@@ -3,4 +3,4 @@
 # There is a commit-hook configured on GitHub. Repository gets pulled and app restarted on commit to 'master'
 # npm install can be temporarily removed as it takes too long...
 
-nohup githooked -p 9000 bash "cd /var/www/rtm/www; git pull origin current && cd /root/thinx-device-api; git pull origin master --recurse-submodules; bash ./tools/install-builders.sh; npm install .; snyk protect; curl https://api.rollbar.com/api/1/deploy/ -F access_token=6aa9f20bef804b75a50338e03830919d -F environment=production -F revision=$(git log -n 1 --pretty=format:\"%H\") -F local_username=$(whoami); pm2 restart ecosystem.json" &
+nohup githooked --ref "refs/heads/master" -p 9000 bash "cd /var/www/rtm/www; git pull origin current && cd /root/thinx-device-api; git pull origin master --recurse-submodules; bash ./tools/install-builders.sh; npm install .; snyk protect; curl https://api.rollbar.com/api/1/deploy/ -F access_token=1de76e75e65e4d85b2ff13be9f719df2 -F environment=production -F revision=$(git log -n 1 --pretty=format:\"%H\") -F local_username=$(whoami); pm2 restart ecosystem.json" &
