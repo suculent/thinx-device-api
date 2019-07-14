1dccbf0b017bf4af6089365f1acc1a3ce72099bc
igraczech <suculent@me.com>
2019-07-14 15:47:36 +0200

    better Redis start

--- services/redis/docker-entrypoint.sh
+++ services/redis/docker-entrypoint.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-source /redis-password
+echo "Starting Redis with password ${REDIS_PASSWORD}"
 
 redis-server --port 6379 --requirepass $REDIS_PASSWORD
 
 redis-cli -a $REDIS_PASSWORD BGSAVE
-
