f36aab6fb9e8e80fabf17cabc4930376ec6fa99a
igraczech <suculent@me.com>
2019-07-24 18:59:04 +0200

    experimentally adjusting tcp timeouts in MQTT module

--- services/broker/docker-entrypoint.sh
+++ services/broker/docker-entrypoint.sh
@@ -4,6 +4,10 @@ set +e
 
 touch /var/log/cron.log
 
+sysctl -w net.ipv4.tcp_keepalive_intvl = 30
+sysctl -w net.ipv4.tcp_keepalive_probes = 8
+sysctl -w net.ipv4.tcp_keepalive_time = 120
+
 echo "Starting cron..."
 
 cron -f &
