439bf4d4f443d7b450bb0c6effc1c9a5254b6930
igraczech <suculent@me.com>
2019-09-11 13:30:35 +0200

    cleaner buildlog, updated user filter doc

--- spec/jasmine/BuildlogSpec.js
+++ spec/jasmine/BuildlogSpec.js
@@ -1,7 +1,7 @@
 describe("Build log", function() {
 
-  var Blog = require("../../lib/thinx/buildlog");
-  var blog = new Blog();
+  var BuildLog = require("../../lib/thinx/buildlog");
+  var blog = new BuildLog();
 
   var envi = require("./_envi.json");
   var owner = envi.oid;
@@ -18,54 +18,6 @@ describe("Build log", function() {
   var session = require("express-session");
   var http = require("http");
   var https = require("https");
-  var WebSocket = require("ws");
-  var ws_done;
-
-  var wsapp = express();
-
-  wsapp.use(function(req, res) {
-    res.send({
-      msg: "TEST"
-    });
-  });
-
-  // WebSocket Server
-  var wserver = http.createServer(wsapp);
-
-  var wss = new WebSocket.Server({
-    port: 7447,
-    server: wserver
-  });
-
-  var _ws = null;
-
-  function noop() {}
-
-  function heartbeat() {
-    this.isAlive = true;
-  }
-
-  wss.on('connection', function connection(ws, req) {
-
-    ws.isAlive = true;
-    ws.on('pong', heartbeat);
-
-    _ws = ws;
-    var location = req.url;
-    console.log("» WSS connection on location: " + location);
-    console.log("» WSS cookie: " + req.headers.cookie);
-    try {
-      ws.send('HELLO');
-    } catch (e) { /* handle error */ }
-
-    ws.on('message', function incoming(message) {
-      console.log('» Websocket message: %s', message);
-    });
-
-    ws.send('READY');
-
-    ws_done();
-  });
 
   it("should be able to initialize", function() {
     expect(blog).toBeDefined();
@@ -89,20 +41,19 @@ describe("Build log", function() {
 
   it("should be able to log", function(done) {
     blog.log(build_id, owner, udid, "Testing build log create...");
-    expect(true).toBe(true);
     done();
-  }, 5000);
+  });
 
   it("should be able to append existing log", function(done) {
     blog.log(build_id, owner, udid, "Testing build log append...");
     done();
-  }, 5000);
+  });
 
-  it("should be able to tail log for build_id", function() {
-    blog.logtail(build_id, require("./_envi.json").oid, _ws,
-      function(err) {
+  it("should be able to tail log for build_id", function(done) {
+    blog.logtail(build_id, require("./_envi.json").oid, _ws, function(err) {
         console.log(err);
-        expect(true).toBe(true);
+        expect(err).toBe(false);
+        done();
       });
   });
 
