3995198541e805f5ace4d24ebd73922a11bd8799
igraczech <suculent@me.com>
2019-09-27 20:04:18 +0200

    fixed validator tests

--- spec/jasmine/ValidatorSpec.js
+++ spec/jasmine/ValidatorSpec.js
@@ -4,45 +4,47 @@ describe("Validator", function() {
   var owner = envi.oid;
   var udid = envi.udid;
 
+  var mock = JSON.stringify({ mock: "mock" });
+  var invalid_mock = JSON.stringify({ unmock: "mock" })+"€~^&*ż{}";
+
   // Validator has only static methods
   const Validator = require('../../lib/thinx/validator');
 
   it("should return TRUE on valid JSON", function() {
-    const mock = JSON.stringify({ mock: "mock" });
-    expect(Validator.json(mock)).toBe(true);
+    expect(Validator.isJSON(mock)).toBe(true);
   });
 
   it("should return FALSE on invalid JSON", function() {
-    const unmock = JSON.stringify({ unmock: "mock" })+"€~^&*ż{}";
-    expect(Validator.json(mock)).toBe(false);
+    expect(Validator.isJSON(invalid_mock)).toBe(false);
   });
 
   it("should trim invalid owner", function() {
     var result = Validator.owner(owner+owner);
-    console.log(result);
+    //console.log("trim invalid owner result: ", result);
     expect(result).toBeDefined();
-    expect(result.equals(owner)).toBe(false);
+    expect(result == owner).toBe(false);
   });
 
   it("should return valid owner", function() {
     var result = Validator.owner(owner);
-    console.log(result);
+    //console.log("result: ", result);
+    //console.log("owner: ", owner);
     expect(result).toBeDefined();
-    expect(result.equals(owner)).toBe(true);
+    expect(result == owner).toBe(true);
   });
 
   it("should trim invalid udid", function() {
     var result = Validator.udid(udid+udid);
-    console.log(result);
+    //console.log(result);
     expect(result).toBeDefined();
-    expect(result.equals(udid)).toBe(false);
+    expect(result == udid).toBe(false);
   });
 
   it("should return valid udid", function() {
     var result = Validator.udid(udid);
-    console.log(result);
+    //console.log(result);
     expect(result).toBeDefined();
-    expect(result.equals(udid)).toBe(true);
+    expect(result == udid).toBe(true);
   });
 
 
