cc72eeeb885447c0da81c169aa040fb0c2282d35
igraczech <suculent@me.com>
2019-09-28 00:15:11 +0200

    improved test coverage by mocking new device for attach/detach ops

--- spec/jasmine/DevicesSpec.js
+++ spec/jasmine/DevicesSpec.js
@@ -2,6 +2,7 @@ describe("Devices", function() {
 
   var generated_key_hash = null;
 
+  var Device = require("../../lib/thinx/device"); var device = new Device();
   var Devices = require("../../lib/thinx/devices");
   var devices = new Devices();
 
@@ -10,6 +11,57 @@ describe("Devices", function() {
   var source_id = envi.sid;
   var udid = envi.udid;
   var ak = envi.ak;
+
+  var crypto = require("crypto");
+  var fake_mac = null;
+
+  crypto.randomBytes(6, function(err, buffer) {
+    var hexa = buffer.toString('hex');
+    fake_mac = hexa.charAt(0) +
+               hexa.charAt(1) + ":" +
+               hexa.charAt(2) +
+               hexa.charAt(3) + ":" +
+               hexa.charAt(4) +
+               hexa.charAt(5);
+  });
+
+  console.log("Testing with udid: ", udid);
+
+  // This UDID is to be deleted at the end of test.
+  var TEST_DEVICE = {
+    mac: fake_mac,
+    firmware: "DeviceSpec.js",
+    version: "1.0.0",
+    checksum: "alevim",
+    push: "forget",
+    alias: "virtual-test-device-3-dynamic",
+    owner: owner,
+    platform: "arduino",
+    udid: null
+  };
+
+  it("should be able to register test device", function(done) {
+    device.register(
+      TEST_DEVICE,
+      ak,
+      null,
+      function(success, response) {
+        if (success === false) {
+          console.log(response);
+          expect(response).toBeDefined();
+          if (response === "owner_found_but_no_key") {
+            done();
+            return;
+          }
+        }
+        //console.log("• DeviceSpec.js: Registration result: ", {response});
+        expect(success).toBe(true);
+        TEST_DEVICE.udid = response.registration.udid;
+        expect(TEST_DEVICE.udid).toBeDefined();
+        console.log("• DevicesSpec.js: Received UDID: " + TEST_DEVICE.udid);
+        done();
+      });
+  }, 15000); // register
 
   // All of this expects successful device registration to safely revoke!
 
@@ -25,7 +77,7 @@ describe("Devices", function() {
   it("should be able to attach a repository to device(s)", function(done) {
     var body = {
       source_id: source_id,
-      udid: udid
+      udid: TEST_DEVICE.udid
     };
     console.log("Attach request...");
     devices.attach(owner, body, function(success, response) {
@@ -39,7 +91,7 @@ describe("Devices", function() {
 
   it("should be able to detach a repository from device(s)", function(done) {
     var body = {
-      udid: udid
+      udid: TEST_DEVICE.udid
     };
     devices.detach(owner, body, function(success, response) {
       expect(success).toBe(true);
@@ -55,7 +107,7 @@ describe("Devices", function() {
   // this device must be created using DeviceSpec.js test
   it("should be able to revoke devices for owner", function(done) {
     var body = {
-      udid: udid
+      udid: TEST_DEVICE.udid
     };
     devices.revoke(owner, body, function(success, response) {
       expect(success).toBe(true);
