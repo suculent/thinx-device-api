85c1a2c1abb007343f5ce72f519453f725b32a7a
igraczech <suculent@me.com>
2019-09-29 22:58:11 +0200

    added missing ACL file to test install

--- thinx.js
+++ thinx.js
@@ -410,8 +410,7 @@ function getDocument(file) {
   }
   // Parser may fail
   try {
-    const filter_doc = JSON.parse(data);
-    return filter_doc;
+    return JSON.parse(data);
   } catch (e) {
     console.log("File may not exist: "+e);
     return false;
@@ -432,7 +431,7 @@ function injectDesign(db, design, file) {
   if (typeof(design) === "undefined") return;
   let design_doc = getDocument(file);
   if (design_doc) {
-    db.insert("_design/" + design, JSON.stringify(design_doc), function(err, body, header) {
+    db.insert("_design/" + design, design_doc, function(err, body, header) {
       logCouchError(err, body, header, "init:design:"+design);
     });
   } else {
@@ -443,7 +442,7 @@ function injectDesign(db, design, file) {
 function injectReplFilter(db, filter, file) {
   let filter_doc = getDocument(file);
   if (filter_doc) {
-    db.insert("_design/repl_filters", JSON.stringify(filter_doc), function(err, body, header) {
+    db.insert("_design/repl_filters", filter_doc, function(err, body, header) {
       logCouchError(err, body, header, "init:repl:"+filter_doc);
     });
   } else {
