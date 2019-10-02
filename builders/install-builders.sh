9d875031b7b8e091c9f46583696ca73625079d64
igraczech <suculent@me.com>
2019-10-02 21:55:44 +0200

    disabled pre-installing builders for faster testing

--- builders/install-builders.sh
+++ builders/install-builders.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Docker will pull those automatically when requested; disabled for faster testing
+
+exit 0
+
 # It would be better to traverse all *-docker-build directories in loop
 # and docker pull just the basename
 
 set -e
 
 docker pull suculent/arduino-docker-build
-
-#docker pull suculent/platformio-docker-build
-#docker pull suculent/micropython-docker-build
-#docker pull suculent/mongoose-docker-build
-#docker pull suculent/nodemcu-docker-build
+docker pull suculent/platformio-docker-build
+docker pull suculent/micropython-docker-build
+docker pull suculent/mongoose-docker-build
+docker pull suculent/nodemcu-docker-build
 
 echo "Builders installed."
