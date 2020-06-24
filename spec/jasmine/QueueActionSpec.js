e610f47611ebbc21d8e3a0959f859c2807774226
Matej Sychra <suculent@me.com>
2020-06-24 02:11:06 +0200

    added topics for unit-testing

---
+++ spec/jasmine/QueueActionSpec.js
@@ -0,0 +1,23 @@
+describe("Queue Action", function() {
+
+    var Action = require("../../lib/thinx/queue_action");
+
+
+
+    // constructor(udid), starts redis client...
+
+    // withString
+
+    // queueWithSource
+
+    // setStarted
+
+    // isRunning
+
+    // isWaiting
+
+    // save
+
+    // delete
+
+}
---
+++ spec/jasmine/QueueSpec.js
@@ -0,0 +1,19 @@
+describe("Queue", function() {
+
+    var Queue = require("../../lib/thinx/queue"); var queue = new Queue();
+
+    // init
+
+    // cron
+
+    // add (multiple)
+
+    // findNext
+
+    // runNext
+
+    // loop
+
+    // should cleanup (findNext)
+
+}
