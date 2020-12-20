de15f476e9bf4157fdaf2dcfa9a2f94549d47627
Matej Sychra <suculent@me.com>
2020-12-20 21:50:18 +0100

    added Dockerfile, documentation for env-vars and WORKER_SECRET to authenticate communication from Builder API

--- services/worker/index.js
+++ services/worker/index.js
@@ -11,6 +11,7 @@ if (typeof(process.env.ROLLBAR_TOKEN) !== "undefined") {
     });
 }
 
+const exec = require("child_process");
 const chalk = require('chalk');
 const error = chalk.bold.red;
 const warning = chalk.keyword('orange');
@@ -20,12 +21,14 @@ const version = require('./package.json').version;
 const schedule = require('node-schedule');
 const io = require('socket.io-client');
 
+const fs = require("fs-extra");
+
 class Worker {
 
-    constructor() {
+    constructor(build_server) {
         this.client_id = null;
         this.is_running = false;
-        this.socket = io('http://localhost:3000');
+        this.socket = io(build_server);
         console.log(chalk`
 {red +=====================================+}
 {red  \\}   {white.bold THiNX Cloud Build Worker ${version} }   {red \\}
@@ -35,22 +38,147 @@ class Worker {
         this.setupScheduler();
     }
 
+    //
+    // Main Logic
+    //
+
+    failJob(sock, job, details) {
+        sock.emit('job-status', {
+            build_id: job.build_id,
+            status: "Failed",
+            details: details
+        });
+        this.is_running = false;
+    }
+
+    runJob(sock, job) {
+
+        if (typeof(job.cmd) === "undefined") {
+            this.failJob(sock, job, "Missing command");
+            return;
+        }
+
+        if (typeof(job.build_id) === "undefined") {
+            this.failJob(sock, job, "Missing build_id");
+            return;
+        }
+
+        if (typeof(job.udid) === "undefined") {
+            this.failJob(sock, job, "Missing udid");
+            return;
+        }
+
+        if (typeof(job.path) === "undefined") {
+            this.failJob(sock, job, "Missing path for device");
+            return;
+        }
+
+        if (typeof(process.env.WORKER_SECRET) !== "undefined") {
+            if (typeof(job.secret) === "undefined") {
+                this.failJob(sock, job, "Missing worker secret");
+                return;
+            } else {
+                if (job.secret.indexOf(process.env.WORKER_SECRET) !== 0) {
+                    this.failJob(sock, job, "Invalid worker authentication");
+                    return;
+                }
+            }
+        }
+
+        this.runShell(job.cmd, job.owner, job.build_id, job.udid, job.path, sock);
+    }
+
+    runShell(CMD, owner, build_id, udid, path, socket) {
+
+		var shell = exec.spawn(CMD, { shell: true });
+
+		console.log("[OID:" + owner + "] [BUILD_STARTED] EXEC from " + __dirname);
+
+		shell.stdout.on("data", (data) => {
+			var string = data.toString();
+			var logline = string;
+			if (logline.substr(logline.count - 3, 1) === "\n\n") {
+				logline = string.substr(0, string.count - 2); // cut trailing newline
+			}
+
+			logline = logline.replace('\r', '').replace('\n', '');
+
+			if (logline !== "\n") {
+				console.log("[" + build_id + "] »» " + logline);
+
+				// just a hack while shell.exit does not work or fails with another error
+				if (logline.indexOf("STATUS OK") !== -1) {
+                    socket.emit('job-status', {
+                        udid: udid,
+                        state: "Success",
+                        completed: true,
+                        build_id: build_id, 
+                        owner: owner,
+                    });
+				}
+			}
+
+			// Something must write to build_path/build.log where the file is tailed from to websocket...
+			//var path = blog.pathForDevice(owner, udid);
+			var build_log_path = path + "/" + build_id + "/build.log";
+			fs.ensureFile(build_log_path, function (err) {
+                if (err) {
+                    console.log(chalk.red("Log file could not be created."));
+                } else {
+                    fs.appendFileSync(build_log_path, logline);
+                }				
+			});
+
+			socket.emit('log', logline);
+
+		}); // end shell on out data
+
+		shell.stderr.on("data", (data) => {
+			var dstring = data.toString();
+			console.log("[STDERR] " + data);
+			if (dstring.indexOf("fatal:") !== -1) {
+                socket.emit('job-status', {
+                    udid: udid,
+                    build_id: build_id, 
+                    state: "Failed"
+                });
+			}
+		}); // end shell on error data
+
+		shell.on("exit", (code) => {
+            console.log("[OID:" + owner + "] [BUILD_COMPLETED] [builder] with code " + code);
+            this.is_running = false;
+            console.log(new Date().getTime(), chalk.bold.green("» ") + chalk.white("Setting running to false, job done..."));
+
+            let state = "Failed";
+			if (code === 0) {
+                state = "Success";
+            }
+            
+            socket.emit('job-status', {
+                udid: udid,
+                build_id: build_id, 
+                state: state
+            });
+		}); // end shell on exit
+	}
+
     setupSocket(socket) {
         
         // Connectivity Events
 
-        socket.on('connect', function () { 
+        socket.on('connect', () => { 
             console.log(new Date().getTime(), chalk.bold.green("» ") + chalk.white("Worker socket connected, registering..."));
             socket.emit('register', "Hello from BuildWorker."); // refactor, post status as well (running, id...)
         });
 
-        socket.on('disconnect', function () {
+        socket.on('disconnect', () => { 
             console.log(new Date().getTime(), chalk.bold.red("» ") + chalk.bold.white("Worker socket disconnected."));
         });
 
         // Business Logic Events
 
-        socket.on('client id', function (data) {
+        socket.on('client id', (data) => { 
             if (this.client_id === null) {
                 console.log(new Date().getTime(), chalk.bold.green("» ") + chalk.white(`We've got assigned initial client id: ${data}`));
             } else {
@@ -60,12 +188,12 @@ class Worker {
             this.client_id = data;
         });
 
-        socket.on('job', function (data) {
+        socket.on('job', (data) => { 
             console.log(new Date().getTime(), chalk.bold.green("» ") + chalk.white(`Incoming job:`), data);
             if (typeof(data.mock) !== "undefined" && data.mock === true) {
                 this.client_id = data;
                 console.log(new Date().getTime(), chalk.bold.green("» ") + chalk.white("Setting running to true..."));
-                console.log(new Date().getTime(), chalk.bold.red("» ") + chalk.bold.white("TODO: Process incoming job by running a working and sending console log through the socket."));
+                console.log(new Date().getTime(), chalk.bold.red("» ") + chalk.bold.white("Processing incoming job..."));
                 this.is_running = true;
                 this.runJob(socket, data);
             }
@@ -80,21 +208,6 @@ class Worker {
         console.log(new Date().getTime(), chalk.bold.green("» ") + chalk.white("Polling loop (1 minute) scheduled."));
     }
 
-    //
-    // Main Logic
-    //
-
-    // Polling Loop
-    runJob(sock, job) {
-        sock.emit('status', {
-            id: job.id, 
-            mock: job.mock,
-            status: 'done' // started, failed, done...
-        });
-        this.is_running = false;
-        console.log(new Date().getTime(), chalk.bold.green("» ") + chalk.white("Setting running to false, job done..."));
-    }
-
     loop() {
         if (!this.is_running) {
             this.socket.emit('poll', 'true');
@@ -104,4 +217,12 @@ class Worker {
     }
 }
 
-new Worker();
+if (typeof(process.env.THINX_SERVER) !== "undefined") {
+    let srv = process.env.THINX_SERVER;
+    console.log(new Date().getTime(), chalk.bold.orange("» ") + chalk.white("Starting build worker against"), srv);
+    new Worker(srv);
+} else {
+    new Worker('http://localhost:3000');
+    console.log(new Date().getTime(), chalk.bold.orange("» ") + chalk.white("Starting build worker without configuration."));
+}
+
--- services/worker/mock-server.js
+++ services/worker/mock-server.js
@@ -3,74 +3,101 @@ const error = chalk.bold.red;
 const warning = chalk.keyword('orange');
 //console.log(error('Error!'));
 //console.log(warning('Warning!'));
+const app = require('express')();
+const port = process.env.PORT || 3000;
+class BuildServer {
 
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+    constructor(maybe_app) {
+        
+        this.http = require('http').Server(app);
+        this.http.listen(port, function () {
+            console.log(chalk`
+        {blue *************************************************}
+        {blue *} {white.bold THiNX BuildServer Manager listening on *:${port} }{blue *}
+        {blue *************************************************}
+            `);
+        });
+        
+        this.io = require('socket.io')(this.http);        
+        this.setupIo(this.io);
+        this.workers = [];
+    }
+    
+    /*
+    app.get('/', function (req, res) {
+        res.sendFile(__dirname + '/index.html');
+    });
+    */
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+    parseSocketMessage(socket, msg) {
 
-function parseMessage(socket, msg) {
+        // Assign client id to new workers...
+        if (0 === msg.indexOf("Hello from BuildWorker")) {
+            socket.emit('client id', socket.id);
+        }
+    }
 
-    // Assign client id to new workers...
-    if (0 === msg.indexOf("Hello from BuildWorker")) {
-        socket.emit('client id', socket.id);
+    setupSocket(socket) {
+
+        // General events
+
+        socket.on('connect', () => {
+            console.log(new Date().getTime(), chalk.bold.green(`»`), chalk.white(`Worker connected: ${socket.id}`));
+            this.workers[socket.id] == true;
+        });
+
+        socket.on('disconnect', () => {
+            console.log(new Date().getTime(), chalk.bold.red(`»`), chalk.white(`Worker disconnected: ${socket.id}`));
+            if (typeof(socket.id) !== "undefined") {
+                this.workers[socket.id] == false;
+            } else {
+                console.log("Socket ID undefined on disconnect.");
+            }
+        });
+
+        // either by directly modifying the `auth` attribute
+        socket.on("connect_error", () => {
+            //socket.auth.token = "abcd";
+            //socket.connect();
+            console.log("connect_error unresolved");
+        });
+
+        // Business Logic events
+
+        socket.on('register', (msg) => {
+            this.io.emit('chat message', "Registration: "+msg);
+            console.log(new Date().getTime(), chalk.bold.green("» ") + chalk.white(`Worker registered: ${socket.id}`));
+            this.parseSocketMessage(socket, msg);
+        });
+
+        socket.on('poll', (msg) => {
+            this.io.emit('job', {
+                mock: true,
+                build_id: 1,
+                owner_id: "m0ck3r1z3r",
+                cmd: "echo hele-vole-ryby",
+                udid: "air-force-one",
+                //path: "/mnt/data/thinx/eu/repos/owner/udid/build_id/"
+                path: "/tmp/thinx-test/"
+            });
+            console.log(new Date().getTime(), chalk.bold.red("» ") + chalk.white("Responding to job request with empty mock (TODO: respond with no-job or real data)."));
+        });
+
+        socket.on('job-status', function (msg) {
+            console.log(new Date().getTime(), chalk.bold.red("» ") + chalk.white("Incoming job status."), msg);
+        });
+
+        socket.on('log', function (msg) {
+            console.log(new Date().getTime(), chalk.bold.red("» ") + chalk.white("LOG: "), msg);
+            // TODO: forward log to web UI, no need to process.
+        });
+    }
+
+    setupIo(io) {
+        io.on('connection', (socket) => {
+            this.setupSocket(socket);
+        });
     }
 }
 
-io.on('connection', function (socket) {
-
-    // console.log(new Date().getTime(), chalk.bold.green("» ") + chalk.white(`Socket connection ${socket.id} initiated.`));
-
-    socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
-        console.log("Chat message:", msg);
-    });
-    
-
-    socket.on('socket message', function (msg) {
-        io.emit('socket message', msg);
-        console.log("Socket message:", msg);
-        parseMessage(socket, msg);
-    });
-
-    socket.on('connect', () => {
-        console.log(new Date().getTime(), chalk.bold.green(`»`), chalk.white(`Worker connected: ${socket.id}`));
-        io.emit('socket message', "Hello from server.");
-    });
-
-    socket.on('disconnect', () => {
-        console.log(new Date().getTime(), chalk.bold.red(`»`), chalk.white(`Worker disconnected: ${socket.id}`));
-    });
-
-    // either by directly modifying the `auth` attribute
-    socket.on("connect_error", () => {
-        //socket.auth.token = "abcd";
-        //socket.connect();
-        console.log("connect_error unresolved");
-    });
-
-    // Business Logic
-
-    socket.on('register', function (msg) {
-        console.log(new Date().getTime(), chalk.bold.green("» ") + chalk.white(`Worker registered: ${socket.id}`));
-        parseMessage(socket, msg);
-    });
-
-    socket.on('poll', function (msg) {
-        io.emit('job', { mock: true, id: 1 });
-        console.log(new Date().getTime(), chalk.bold.red("» ") + chalk.white("Responding to job request with empty mock (TODO: respond with no-job or real data)."));
-    });
-
-});
-
-http.listen(port, function () {
-    console.log(chalk`
-{blue *************************************************}
-{blue *} {white.bold THiNX BuildServer Manager listening on *:${port} }{blue *}
-{blue *************************************************}
-    `);
-});
+new BuildServer();
