19d9ea92646a97856e85f8c8f3c88629c1ae2c2d
Matej Sychra <suculent@me.com>
2020-12-25 18:01:30 +0100

    apk uses del; chmod for devsec build result

--- services/worker/devsec/build.sh
+++ services/worker/devsec/build.sh
@@ -1,12 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ $(uname -s) == "Darwin" ]]; then
-  echo "Building DevSec binary for Mac OS..."
-  g++ main.cpp devsec.cpp -o ../../devsec
-elif [[ $(uname -s) == "Linux" ]]; then
-  echo "Building DevSec binary for Linux..."
-  g++ -std=c++11 main.cpp devsec.cpp -o ../../devsec
-else
-  echo "Unsupported platform for building."
-  exit 1
-fi
+echo "Building DevSec binary for Linux..."
+g++ -std=c++11 main.cpp devsec.cpp -o ../devsec
