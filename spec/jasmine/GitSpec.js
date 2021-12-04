83a167d141cd229ed3a19317ddb745f55366f785
Matej Sychra <suculent@me.com>
2021-12-04 11:07:12 +0100

    extended timeout; removed codacy reporter (fails as this is no git repo)

--- spec/jasmine/GitSpec.js
+++ spec/jasmine/GitSpec.js
@@ -5,9 +5,10 @@ describe("Git", function() {
 
     it("should be able to fetch", function(done) {
         let git = new Git();
-        let SHELL_FETCH = "git pull --recurse-submodules";
-        let local_path = __dirname;
-        let success = git.fetch("0fbe2e3d9326d4b318476c8a26b4ac93d38cf7da69b04204822a3bff102dc622", SHELL_FETCH, local_path);
+        let success = git.fetch(
+            "0fbe2e3d9326d4b318476c8a26b4ac93d38cf7da69b04204822a3bff102dc622", 
+            "git pull --recurse-submodules", 
+            __dirname);
         console.log("git fetch result", success);
         expect(success); // may fail until <owner> is valid
         done();
