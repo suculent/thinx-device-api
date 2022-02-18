3fff81d0c7556ed048ee6e20e81995addecfb9c2
Matej Sychra <suculent@me.com>
2022-02-18 23:57:40 +0100

    test spec fixes in sanitka; improving coverage, designing replication filter creation (getDocument in database.js is not entirely called, files not found)

--- spec/jasmine/SanitkaSpec.js
+++ spec/jasmine/SanitkaSpec.js
@@ -16,7 +16,7 @@ describe("Sanitka", function () {
 
   it("should de-escape (delete) dangerous shell characters \", \', ;", function () {
     var s = sanitka.deescape("\"\';;;\"");
-    expect(s.length).to.be(null);
+    expect(s.length).to.equal(null);
   });
 
   it("should accept valid owner", function () {
@@ -25,9 +25,9 @@ describe("Sanitka", function () {
     expect(result).to.equal(input);
   });
 
-  it("should reject valid owner", function () {
+  it("should reject invalid owner", function () {
     var result = sanitka.owner("invalid-owner");
-    expect(result).to.be(null);
+    expect(result).to.equal(null);
   });
 
   it("should accept valid iOS push token", function () {
@@ -39,19 +39,21 @@ describe("Sanitka", function () {
   it("should accept valid GCM push token", function () {
     let input = "akO1-XdQYgk:APA91bHmgm_K500RVhexcxFVoczhp5RuMSKC07kOJB7T31xq2_a9tkUAFVGQNwtZ2JORj79lDRI0ow-nP17y82GD1zTWJTEnyjNMas_qNUKxBot1P-vM6v-BW7sqcISak8sXMK91WfmH";
     var result = sanitka.pushToken(input);
+    console.log("test INN:", input);
+    console.log("test OUT:", result);
     expect(result).to.equal(input);
   });
 
   it("should reject invalid iOS push token", function () {
     let input = "31b1f6bf498d7cec463ff2588aca59a52df6f880e60e8d4d6bcda0d8e6e87823'";
     var result = sanitka.pushToken(input);
-    expect(result).to.be(null);
+    expect(result).to.equal(null);
   });
 
   it("should rejec invalid GCM push token", function () {
     let input = "akO1'XdQYgk!APA91bHmgm_K500RVhexcxFVoczhp5RuMSKC07kOJB7T31xq2_a9tkUAFVGQNwtZ2JORj79lDRI0ow-nP17y82GD1zTWJTEnyjNMas_qNUKxBot1P-vM6v-BW7sqcISak8sXMK91WfmH";
     var result = sanitka.pushToken(input);
-    expect(result).to.be(null);
+    expect(result).to.equal(null);
   });
 
   it("should accept valid API Key", function () {
