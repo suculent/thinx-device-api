bf5f6ec5e376f89dc8ae348282215388ab139e2b
Matej Sychra <suculent@me.com>
2022-02-19 01:21:36 +0100

    removed useless creation, reformatted but the DB does not get created properly now

--- spec/jasmine/RepositorySpec.js
+++ spec/jasmine/RepositorySpec.js
@@ -8,20 +8,20 @@ describe("Repository Watcher", function() {
 
   var watcher = new Repository();
 
-  console.log("Using repo_path: "+repo_path);
+  console.log("[test] Watcher is using repo_path: "+repo_path);
 
   it("should be able to initialize", function() {
     watcher = new Repository();
-    expect(watcher).to.be.an('object');;
+    expect(watcher).to.be.an('object');
   });
 
   watcher.callback = function(err) {
     // watcher exit_callback
-    console.log("Callback 1");
+    console.log("Callback 1", err);
   };
   watcher.exit_callback = function(err) {
     // watcher exit_callback
-    console.log("Callback 2");
+    console.log("Callback 2", err);
   };
 
 });
