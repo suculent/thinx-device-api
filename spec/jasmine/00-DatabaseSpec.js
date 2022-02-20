c92bc0f28e98d828a3945df41c8848261b7a1159
Matej Sychra <suculent@me.com>
2022-02-20 16:48:15 +0100

    integrated database initialization with callback to init rest of app (not necessarily, but for simplicity everything was taken... can be refactored later but should not fail)

--- spec/jasmine/00-DatabaseSpec.js
+++ spec/jasmine/00-DatabaseSpec.js
@@ -1,12 +1,20 @@
-describe("Database", function() {
+describe("Database", function () {
 
   var Database = require("../../lib/thinx/database");
   var database = new Database();
-  
-  it("should start and create initial DBs", function(done) {    
-    database.init(() => {
+
+  it("should start and create initial DBs", function (done) {
+    database.init((err, result) => {
+      console.log("DB init test err, result", err, result); // remove after turning to expect
       done();
     });
   }, 5000);
+
+  it("should run compactor", function (done) {
+    database.compactDatabases((success) => {
+      expect(success).toBe.be.true;
+      done();
+    });
+  }, 10000);
 
 });
