5c6685820a21f11cdc465f6203c8f4aa52a33017
Matej Sychra <suculent@me.com>
2022-03-11 19:06:53 +0100

    latest build is stable; smoke-test before merge

--- install/cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Copyright © 2020 Richard Prajer
-# This script should be run in /mnt/local/repos or /mnt/data/repos folders (on the owner-id level)
-
-for dir in */*; do find $PWD/$dir -type d -printf "%T@\t%p\n" | sort -t $'\t' -g | sed '1d; $d' | cut -d $'\t' -f 2 | xargs rm -r -- ; done
