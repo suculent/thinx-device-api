317d00d06bbafe904a6dc6225542eb6f1e52348c
Matej Sychra <suculent@me.com>
2022-03-15 18:17:41 +0100

    log double space fix

--- thinx.js
+++ thinx.js
@@ -113,10 +113,10 @@ db.init((/* db_err, dbs */) => {
   let now = new Date();
   stats.get_all_owners();
   let then = new Date();
-  console.log(`ℹ️  [info] [core] get_all_owners took ${then - now} seconds.`);
+  console.log(`ℹ️ [info] [core] cached all owners in ${then - now} seconds.`);
   setInterval(() => {
     stats.aggregate();
-    console.log("✅  [info] Aggregation jobs completed.");
+    console.log("✅ [info] Aggregation jobs completed.");
   }, 86400 * 1000 / 2);
 
   //
@@ -223,7 +223,7 @@ db.init((/* db_err, dbs */) => {
 
   // Legacy HTTP support for old devices without HTTPS proxy
   let server = http.createServer(app).listen(app_config.port, "0.0.0.0", function () {
-    console.log(`ℹ️  [info] HTTP API started on port ${app_config.port}`);
+    console.log(`ℹ️ [info] HTTP API started on port ${app_config.port}`);
     let end_timestamp = new Date().getTime() - start_timestamp;
     let seconds = Math.ceil(end_timestamp / 1000);
     console.log("⏱ [profiler] Startup phase took:", seconds, "seconds");
