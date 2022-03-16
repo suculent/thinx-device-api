4bbd50c343404fbbcf5a1d082a493ebe6ac4b92f
Matej Sychra <suculent@me.com>
2022-03-16 17:15:40 +0100

    seems like undeployed guard

--- thinx.js
+++ thinx.js
@@ -432,6 +432,11 @@ db.init((/* db_err, dbs */) => {
       return;
     }
 
+    if (typeof(req.session.cookie) === "undefined") {
+      console.log("[error] No cookie on wss connection! Should bear thx-session");
+      return;
+    }
+
     // extract socket id and owner_id from pathname, also removing slashes
     console.log("Request URL:", req.url);
     let path_elements = req.url.split('/');
