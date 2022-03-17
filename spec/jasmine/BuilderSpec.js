61f793b248e26989c047bd04271b9a335a2ac148
Matej Sychra <suculent@me.com>
2022-03-17 18:45:28 +0100

    131 specs passed, starting to expand tests to build phase

--- spec/jasmine/BuilderSpec.js
+++ spec/jasmine/BuilderSpec.js
@@ -1,3 +1,5 @@
+const Notifier = require('../../lib/thinx/notifier');
+
 var expect = require('chai').expect;
 
 describe("Builder", function() {
@@ -12,6 +14,10 @@ describe("Builder", function() {
   var apikey = envi.ak;
   var build_id = envi.build_id; // "f168def0-597f-11e7-a932-014d5b00c004";
   var source_id = envi.sid;
+
+  let notifiers = [
+    new Notifier()
+  ];
 
   it("should be able to initialize", function() {
     expect(builder).to.be.a('object');
@@ -29,6 +35,7 @@ describe("Builder", function() {
       build,
       [], // notifiers
       function(success, message, build_id) {
+        console.log("[spec] build dry", {success}, {message}, {build_id});
         done();
       }, // callback
       worker
@@ -45,8 +52,9 @@ describe("Builder", function() {
     builder.build(
       owner, 
       build, 
-      [], // notifiers
-      function(success, message, build_id) {
+      notifiers,
+      function(success, message, build_id2) {
+        console.log("[spec] build dry", {success}, {message}, {build_id2});
         done();
       }, // callback
       worker
@@ -65,4 +73,12 @@ describe("Builder", function() {
     //console.log(JSON.stringify(extensions));
   });
 
+  it("should not fail on build", function(done) {
+    let br = {};
+    let transmit_key = "mock-transmit-key";
+      builder.run_build(br, notifiers, function(success, result) {
+        console.log("[spec] build TODO", {success}, {result});
+        done();
+      }, transmit_key);
+    });
 });
