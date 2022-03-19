164812954d0cc0b4f1013669f7fd4986f826cf81
Matej Sychra <suculent@me.com>
2022-03-19 17:38:36 +0100

    refactoring validator to sanitka, dropping duplicate code – both should work the same: return false or value

--- spec/jasmine/ValidatorSpec.js
+++ spec/jasmine/ValidatorSpec.js
@@ -18,36 +18,4 @@ describe("Validator", function() {
   it("should return FALSE on invalid JSON", function() {
     expect(Validator.isJSON(invalid_mock)).to.equal(false);
   });
-
-  it("should reject invalid owner", function() {
-    var result = Validator.owner(owner+owner); // expect only boolean result
-    expect(result).to.equal(false);
-  });
-
-  it("should return valid owner", function() {
-    var result = Validator.owner(owner); // TODO: FIXME: expect only boolean result!
-    expect(result).to.be.a('string');
-    expect(result == owner).to.be.true;
-  });
-
-  it("should reject long udid", function() {
-    var result = Validator.udid(udid+udid);
-    expect(result).to.equal(false);
-  });
-
-  it("should reject undefined", function() {
-    var result = Validator.udid(undefined);
-    expect(result).to.equal(false);
-  });
-
-  it("should survive infinite loop with zero-width match", function() {
-    var result = Validator.udid("");
-    expect(result).to.equal(false);
-  });
-
-  it("should return valid udid", function() {
-    var result = Validator.udid(udid);
-    expect(result).to.be.a('string');
-    expect(result == udid).to.be.true;
-  });
 });
