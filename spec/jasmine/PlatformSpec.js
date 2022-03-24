13f9fd1f7c38d3f1c98bbcea50d28c7d892258ce
Matej Sychra <suculent@me.com>
2022-03-24 14:47:43 +0100

    expanding platform detector coverage

--- spec/jasmine/PlatformSpec.js
+++ spec/jasmine/PlatformSpec.js
@@ -2,6 +2,10 @@ describe("Platform", function() {
 
     let Platform = require("../../lib/thinx/platform");
     let path = "./spec/test_repositories/arduino/";
+    let path_ino = "./spec/test_repositories/thinx-firmware-esp32-ino/";
+    let path_lua = "./spec/test_repositories/thinx-firmware-esp32-lua/";
+    let path_pio = "./spec/test_repositories/thinx-firmware-esp32-pio/";
+    let path_upy = "./spec/test_repositories/thinx-firmware-esp32-upy/";
     
     var expect = require('chai').expect;
 
@@ -20,6 +24,47 @@ describe("Platform", function() {
     it("should be able to infer platform from repository contents", function(done) {
         Platform.getPlatform(path, function(success, result) {
           expect(result).to.be.a('string');
+          done();
+        });
+    }, 15000);
+
+    it("should not fail on invalid local path", function(done) {
+        let undefined_path;
+        Platform.getPlatform(undefined_path, function(success, result) {
+          expect(success).to.equal('false');
+          console.log("platform detection result on invalid path:", result);
+          done();
+        });
+    }, 15000);
+
+    it("should detect arduino", function(done) {
+        Platform.getPlatform(path_ino, function(success, result) {
+          expect(success).to.equal('true');
+          console.log("platform detection result:", result);
+          done();
+        });
+    }, 15000);
+
+    it("should detect lua", function(done) {
+        Platform.getPlatform(path_lua, function(success, result) {
+          expect(success).to.equal('true');
+          console.log("platform detection result:", result);
+          done();
+        });
+    }, 15000);
+
+    it("should detect pio", function(done) {
+        Platform.getPlatform(path_pio, function(success, result) {
+          expect(success).to.equal('true');
+          console.log("platform detection result:", result);
+          done();
+        });
+    }, 15000);
+
+    it("should detect micropython", function(done) {
+        Platform.getPlatform(path_upy, function(success, result) {
+          expect(success).to.equal('true');
+          console.log("platform detection result:", result);
           done();
         });
     }, 15000);
