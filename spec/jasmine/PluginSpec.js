557f806faeef2b9c116abd40529249261d8f6b31
Matej Sychra <suculent@me.com>
2022-03-24 19:06:23 +0100

    testing support for multiple plugins

--- spec/jasmine/PluginSpec.js
+++ spec/jasmine/PluginSpec.js
@@ -1,15 +1,19 @@
 var expect = require('chai').expect;
 const Plugins = require('../../lib/thinx/plugins');
 let config = __dirname + "/../../lib/thinx/plugins/plugins.json";
+let empty = __dirname + "/../empty.json";
 
 describe("Plugins", function () {
 
-    // init
     it("should not fail", async function () {
         let manager = new Plugins(this);
         await manager.loadFromConfig(config);
     });
 
+    it("should fail safely", async function () {
+        let manager = new Plugins(this);
+        await manager.loadFromConfig(empty);
+    });
     
     it("should detect sample platform positively", async function () {
         let manager = new Plugins(this);
@@ -22,6 +26,16 @@ describe("Plugins", function () {
         path = "../test_repositories/thinx-firmware-js";
         result = manager.plugins['sample'].check(path);
         expect(result).to.equal(false);
+    });
+
+    
+    it("should be able to use all plugins at once", async function () {
+        let manager = new Plugins(this);
+        await manager.loadFromConfig(config);
+        
+        let path = "../test_repositories/arduino";
+        let result = await manager.use(path);
+        expect(result).to.equal('arduino');
     });
 
 });
