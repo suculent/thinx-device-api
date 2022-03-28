b84c4e49a55919d37ee629a893828b15defb8e91
Matej Sychra <suculent@me.com>
2022-03-28 16:13:36 +0200

    merged build and notify steps, extracting build_id on start

--- spec/jasmine/BuilderSpec.js
+++ spec/jasmine/BuilderSpec.js
@@ -22,6 +22,8 @@ describe("Builder", function () {
   var source_id = envi.sid;
   var ak = envi.ak;
 
+  var spec_build_id = null;
+
   // This UDID is to be deleted at the end of test.
   var TEST_DEVICE_5 = {
     mac: "AA:BB:CC:EE:00:05",
@@ -135,67 +137,48 @@ describe("Builder", function () {
 
     let transmit_key = "mock-transmit-key";
     builder.run_build(build_request, [] /* notifiers */, function (success, result) {
-      console.log("[spec] build TODO", { success }, { result });
       expect(success).to.equal(true);
+      //expect(result).to.equal('build_started');
+      console.log("[spec] run_build result:", {result});
+      spec_build_id = result.build_id;
+      // result contains build_id for notification...
+
+      var test_build_id = spec_build_id;
+      var test_commit_id = "mock_commit_id";
+      var test_repo = "https://github.com/suculent/thinx-firmware-esp8266-pio.git";
+      var test_binary = "/tmp/nothing.bin";
+      var test_udid = TEST_DEVICE_5.udid;
+      var sha = "one-sha-256-pls";
+      var owner_id = envi.oid;
+      var status = "TESTING_NOTIFIER";
+      var platform = "platformio";
+      var version = "thinx-firmware-version-1.0";
+
+      let job_status = {
+        build_id: test_build_id,
+        commit: test_commit_id,
+        thx_version: "1.5.X",
+        git_repo: test_repo,
+        outfile: test_binary,
+        udid: test_udid,
+        sha: sha,
+        owner: owner_id,
+        status: status,
+        platform: platform,
+        version: version,
+        md5: "md5-mock-hash",
+        env_hash: "cafebabe"
+      };
+
+      let notifier = new Notifier();
+
+      notifier.process(job_status, (result) => {
+        console.log("ℹ️ [info] Notifier's Processing result:", result);
+        done();
+      });
+      
       done();
     }, transmit_key);
-  });
-
-  it("should be able to send a notification", function (done) {
-
-    /* this is how job_status is generated inside the global build script:
-    # Inside Worker, we don't call notifier, but just post the results into shell... THiNX builder must then call the notifier itself (or integrate it later)
-    JSON=$(jo \
-    build_id=${BUILD_ID} \
-    commit=${COMMIT} \
-    thx_version=${THX_VERSION} \
-    git_repo=${GIT_REPO} \
-    outfile=$(basename ${OUTFILE}) \
-    udid=${UDID} \
-    sha=${SHA} \
-    owner=${OWNER_ID} \
-    status=${STATUS} \
-    platform=${PLATFORM} \
-    version=${THINX_FIRMWARE_VERSION} \
-    md5=${MD5} \
-    env_hash=${ENV_HASH} \
-    )
-    */
-
-    // Hey, this should be JUST a notification, no destructives.
-    var test_build_id = "mock_build_id";
-    var test_commit_id = "mock_commit_id";
-    var test_repo = "https://github.com/suculent/thinx-firmware-esp8266-pio.git";
-    var test_binary = "/tmp/nothing.bin";
-    var test_udid = TEST_DEVICE_5.udid;
-    var sha = "one-sha-256-pls";
-    var owner_id = envi.oid;
-    var status = "TESTING_NOTIFIER";
-    var platform = "platformio";
-    var version = "thinx-firmware-version-1.0";
-
-    let job_status = {
-      build_id: test_build_id,
-      commit: test_commit_id,
-      thx_version: "1.5.X",
-      git_repo: test_repo,
-      outfile: test_binary,
-      udid: test_udid,
-      sha: sha,
-      owner: owner_id,
-      status: status,
-      platform: platform,
-      version: version,
-      md5: "md5-mock-hash",
-      env_hash: "cafebabe"
-    };
-
-    let notifier = new Notifier();
-
-    notifier.process(job_status, (result) => {
-      console.log("ℹ️ [info] Notifier's Processing result:", result);
-      done();
-    });
   });
 
   it("should fetch last apikey", function (done) {
