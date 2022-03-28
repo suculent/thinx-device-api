f2f1e5454d70e85bc8a98d811b55b611384e4f9d
Matej Sychra <suculent@me.com>
2022-03-28 19:42:26 +0200

    added missing spec callback; cleaning debug logs

--- spec/jasmine/DeploymentSpec.js
+++ spec/jasmine/DeploymentSpec.js
@@ -1,4 +1,4 @@
-describe("Deployer", function() {
+describe("Deployer", function () {
 
   var expect = require('chai').expect;
   var Deployment = require('../../lib/thinx/deployment');
@@ -15,43 +15,44 @@ describe("Deployer", function() {
     udid: envi.udid
   };
 
-  it("should be able to initialize", function() {
+  it("should be able to initialize", function () {
     deploy.initWithOwner(owner);
     expect(deploy).to.be.a('object');
   });
 
-  it("should be able to init with device", function() {
+  it("should be able to init with device", function () {
     deploy.initWithDevice(device);
     expect(true).to.equal(true);
   });
 
-  it("should be able to return path for device", function() {
+  it("should be able to return path for device", function () {
     var repo_path = deploy.pathForDevice(owner, udid);
     expect(repo_path).to.be.a('string');
   });
 
-  it("should be able to return latest firmware path", function() {
-    var firmwarePath = deploy.latestFirmwarePath(device.owner);
-    expect(firmwarePath).to.equal(false);
+  it("should be able to return latest firmware path", function (done) {
+    deploy.latestFirmwarePath(device.owner, envi.udid, (path) => {
+      expect(path).to.equal(false);
+      done();
+    });
   });
 
-  it("should be able to tell whether update is available for device",
-    function() {
-      var result = deploy.hasUpdateAvailable(device);
-      expect(result).to.equal(false);
+  it("should be able to tell whether update is available for device", function () {
+    var result = deploy.hasUpdateAvailable(device);
+    expect(result).to.equal(false);
   });
 
-  it("should be able to return latest firmware envelope", function() {
+  it("should be able to return latest firmware envelope", function () {
     var firmwareUpdateDescriptor = deploy.latestFirmwareEnvelope(device.owner, device.udid);
     expect(firmwareUpdateDescriptor).to.equal(false);
   });
 
-  it("should be able to return update support for platform", function() {
+  it("should be able to return update support for platform", function () {
     var result = deploy.platformSupportsUpdate(device);
     expect(result).to.equal(false);
   });
 
-  it("should be able to validate if device has update available", function() {
+  it("should be able to validate if device has update available", function () {
     var result = deploy.validateHasUpdateAvailable(device);
     expect(result).to.equal(false);
   });
