e0b6bf3b3a7e3b1366122f5bf24bc28c6c80be75
Matej Sychra <suculent@me.com>
2022-03-30 01:35:10 +0200

    potential test coverage improvement -> lgtm fixes

--- spec/jasmine/00-AppSpec.js
+++ spec/jasmine/00-AppSpec.js
@@ -2,11 +2,15 @@ const THiNX = require("../../thinx-core.js");
 
 describe("App", function () {
 
-  it("App start should not fail.", function (done) {
+  it("App class should not fail.", function (done) {
     let thx = new THiNX();
     thx.init(() => {
       done();
     });
   }, 20000);
+
+  it("App start should not fail.", function() {
+    require('../../thinx.js');
+  });
 
 });
