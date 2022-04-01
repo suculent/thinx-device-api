e30447fd84f48823f82e582a7ecdc6b9d82f7187
Matej Sychra <suculent@me.com>
2022-04-01 10:32:20 +0200

    fixed some statuses; see what happens when sharing the thx object per spec (from 26 errors! seems like rising)

--- spec/jasmine/01-AppDeviceSpec.js
+++ spec/jasmine/01-AppDeviceSpec.js
@@ -20,6 +20,7 @@ describe("Devices", function () {
   });
 
   it("GET /api/user/devices", function (done) {
+    console.log("GET /api/user/devices");
     chai.request(thx.app)
       .get('/api/user/devices')
       .end((err, res) => {
@@ -30,7 +31,8 @@ describe("Devices", function () {
       });
   }, 20000);
 
-  it("GET /api/device/data/" + envi.oid, function (done) {
+  it("GET /api/user/device/data/:udid" + envi.oid, function (done) {
+    console.log("[chai] GET /api/user/device/data/:udid");
     chai.request(thx.app)
       .get('/api/user/device/data/' + envi.oid)
       .end((err, res) => {
@@ -41,113 +43,99 @@ describe("Devices", function () {
   }, 20000);
 
   it("POST /api/device/edit", function (done) {
-    let thx = new THiNX();
-    thx.init(() => {
-      chai.request(thx.app)
-        .post('/api/device/edit')
-        .send({ changes: { alias: "edited-alias" } })
-        .end((err, res) => {
-          console.log("[chai] response:", res.text, " status:", res.status);
-          //expect(res.status).to.equal(200);
-          //expect(res.text).to.be.a('string');
-          done();
-        });
-    });
+    console.log("[chai] POST /api/device/edit");
+    chai.request(thx.app)
+      .post('/api/device/edit')
+      .send({ changes: { alias: "edited-alias" } })
+      .end((err, res) => {
+        console.log("[chai] response:", res.text, " status:", res.status);
+        //expect(res.status).to.equal(200);
+        //expect(res.text).to.be.a('string');
+        done();
+      });
   }, 20000);
 
   it("POST /api/device/detach", function (done) {
-    let thx = new THiNX();
-    thx.init(() => {
-      chai.request(thx.app)
-        .post('/api/device/detach')
-        .send({ udid: envi.oid })
-        .end((err, res) => {
-          console.log("[chai] response:", res.text, " status:", res.status);
-          //expect(res.status).to.equal(200);
-          //expect(res.text).to.be.a('string');
-          done();
-        });
-    });
+    console.log("[chai] POST /api/device/detach");
+    chai.request(thx.app)
+      .post('/api/device/detach')
+      .send({ udid: envi.oid })
+      .end((err, res) => {
+        console.log("[chai] response:", res.text, " status:", res.status);
+        //expect(res.status).to.equal(200);
+        //expect(res.text).to.be.a('string');
+        done();
+      });
   }, 20000);
 
   it("POST /api/device/mesh/attach", function (done) {
-    let thx = new THiNX();
-    thx.init(() => {
-      chai.request(thx.app)
-        .post('/api/device/mesh/attach')
-        .send({ udid: envi.oid })
-        .end((err, res) => {
-          console.log("[chai] response:", res.text, " status:", res.status);
-          //expect(res.status).to.equal(200);
-          //expect(res.text).to.be.a('string');
-          done();
-        });
-    });
+    console.log("[chai] POST /api/device/mesh/attach");
+    chai.request(thx.app)
+      .post('/api/device/mesh/attach')
+      .send({ udid: envi.oid })
+      .end((err, res) => {
+        console.log("[chai] response:", res.text, " status:", res.status);
+        //expect(res.status).to.equal(200);
+        //expect(res.text).to.be.a('string');
+        done();
+      });
   }, 20000);
 
   // POST /api/device/mesh/detach
   it("POST /api/device/mesh/detach", function (done) {
-    let thx = new THiNX();
-    thx.init(() => {
-      chai.request(thx.app)
-        .post('/api/device/mesh/detach')
-        .send({ udid: envi.oid })
-        .end((err, res) => {
-          console.log("[chai] response:", res.text, " status:", res.status);
-          //expect(res.status).to.equal(200);
-          //expect(res.text).to.be.a('string');
-          done();
-        });
-    });
+    console.log("[chai] POST /api/device/mesh/detach");
+    chai.request(thx.app)
+      .post('/api/device/mesh/detach')
+      .send({ udid: envi.oid })
+      .end((err, res) => {
+        console.log("[chai] response:", res.text, " status:", res.status);
+        //expect(res.status).to.equal(200);
+        //expect(res.text).to.be.a('string');
+        done();
+      });
   }, 20000);
 
   it("POST /api/device/data", function (done) {
-    let thx = new THiNX();
-    thx.init(() => {
-      console.log("[chai] request /api/device/data");
-      chai.request(thx.app)
-        .post('/api/device/data')
-        .send({ udid: envi.oid })
-        .end((err, res) => {
-          console.log("[chai] response /api/device/data:", res.text, " status:", res.status);
-          //expect(res.status).to.equal(200);
-          //expect(res.text).to.be.a('string');
-          done();
-        });
-    });
+    console.log("[chai] POST /api/device/data");
+    chai.request(thx.app)
+      .post('/api/device/data')
+      .send({ udid: envi.oid })
+      .end((err, res) => {
+        console.log("[chai] response /api/device/data:", res.text, " status:", res.status);
+        //expect(res.status).to.equal(200);
+        //expect(res.text).to.be.a('string');
+        done();
+      });
   }, 20000);
 
   it("POST /api/device/revoke", function (done) {
-    let thx = new THiNX();
-    thx.init(() => {
-      chai.request(thx.app)
-        .post('/api/device/revoke')
-        .send({ udid: envi.oid })
-        .end((err, res) => {
-          console.log("[chai] response:", res.text, " status:", res.status);
-          //expect(res.status).to.equal(200);
-          //expect(res.text).to.be.a('string');
-          done();
-        });
-    });
+    console.log("[chai] POST /api/device/revoke");
+    chai.request(thx.app)
+      .post('/api/device/revoke')
+      .send({ udid: envi.oid })
+      .end((err, res) => {
+        console.log("[chai] response:", res.text, " status:", res.status);
+        //expect(res.status).to.equal(200);
+        //expect(res.text).to.be.a('string');
+        done();
+      });
   }, 20000);
 
-});
+  //
+  // Device Configuration
+  //
 
-describe("Device Configuration", function () {
   // push device configuration over MQTT
   it("POST /api/device/push", function (done) {
-    let thx = new THiNX();
-    thx.init(() => {
-      chai.request(thx.app)
-        .post('/api/device/push')
-        .send({ key: "value" })
-        .end((err, res) => {
-          console.log("[chai] response:", res.text, " status:", res.status);
-          //expect(res.status).to.equal(200);
-          //expect(res.text).to.be.a('string');
-          done();
-        });
-    });
+    console.log("[chai] POST /api/device/push");
+    chai.request(thx.app)
+      .post('/api/device/push')
+      .send({ key: "value" })
+      .end((err, res) => {
+        console.log("[chai] response:", res.text, " status:", res.status);
+        //expect(res.status).to.equal(200);
+        //expect(res.text).to.be.a('string');
+        done();
+      });
   }, 20000);
 });
