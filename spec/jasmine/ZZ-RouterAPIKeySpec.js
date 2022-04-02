c947f1bbf1c95c79cc2c78b65bc3afa845d312db
Matej Sychra <suculent@me.com>
2022-04-02 17:11:50 +0200

    API Key revocation spec fix (covering single and multiple revocation with correct input)

--- spec/jasmine/ZZ-RouterAPIKeySpec.js
+++ spec/jasmine/ZZ-RouterAPIKeySpec.js
@@ -22,45 +22,37 @@ describe("API Keys (noauth)", function () {
 
     // create
     it("POST /api/user/apikey", function (done) {
-        console.log("[chai] request /api/user/apikey");
         chai.request(thx.app)
             .post('/api/user/apikey')
             .send({
                 'alias': 'mock-apikey-alias'
             })
             .end((err, res) => {
-                console.log("[chai] response /api/user/apikey:", res.text, " status:", res.status);
-                //expect(res.status).to.equal(200);
-                //expect(res.text).to.be.a('string');
+                expect(res.status).to.equal(403);
                 done();
             });
     }, 20000);
 
     // revoke
     it("POST /api/user/apikey/revoke", function (done) {
-        console.log("[chai] request /api/user/apikey/revoke");
         chai.request(thx.app)
             .post('/api/user/apikey/revoke')
             .send({
                 'alias': 'mock-apikey-alias'
             })
             .end((err, res) => {
-                console.log("[chai] response /api/user/apikey/revoke:", res.text, " status:", res.status);
-                //expect(res.status).to.equal(200);
-                //expect(res.text).to.be.a('string');
+                expect(res.status).to.equal(403);
                 done();
             });
     }, 20000);
 
     // list
     it("GET /api/user/apikey/list", function (done) {
-        console.log("[chai] request GET /api/user/apikey/list");
         chai.request(thx.app)
             .get('/api/user/apikey/list')
             .end((err, res) => {
                 console.log("[chai] response /api/user/apikey/list:", res.text, " status:", res.status);
-                //expect(res.status).to.equal(200);
-                //expect(res.text).to.be.a('string');
+                expect(res.status).to.equal(403);
                 done();
             });
     }, 20000);
@@ -103,9 +95,11 @@ describe("API Keys (JWT)", function () {
     // Unauthenticated
     //
 
+    var created_api_key = null;
+    var created_api_key_2 = null;
+
     // create
-    it("POST /api/user/apikey", function (done) {
-        console.log("[chai] request /api/user/apikey (JWT)");
+    it("POST /api/user/apikey (1)", function (done) {
         chai.request(thx.app)
             .post('/api/user/apikey')
             .set('Authorization', jwt)
@@ -113,26 +107,72 @@ describe("API Keys (JWT)", function () {
                 'alias': 'mock-apikey-alias'
             })
             .end((err, res) => {
-                console.log("[chai] response /api/user/apikey (JWT):", res.text, " status:", res.status);
-                //expect(res.status).to.equal(200);
-                //expect(res.text).to.be.a('string');
+                //  {"success":true,"api_key":"9b7bd4f4eacf63d8453b32dbe982eea1fb8bbc4fc8e3bcccf2fc998f96138629","hash":"0a920b2e99a917a04d7961a28b49d05524d10cd8bdc2356c026cfc1c280ca22c"}
+                expect(res.status).to.equal(200);
+                let j = JSON.parse(res.text);
+                expect(j.success).to.equal(true);
+                expect(j.api_key).to.be.a('string');
+                expect(j.hash).to.be.a('string');
+                created_api_key = j.hash;
+                done();
+            });
+    }, 20000);
+
+    it("POST /api/user/apikey (2)", function (done) {
+        chai.request(thx.app)
+            .post('/api/user/apikey')
+            .set('Authorization', jwt)
+            .send({
+                'alias': 'mock-apikey-alias-2'
+            })
+            .end((err, res) => {
+                //  {"success":true,"api_key":"9b7bd4f4eacf63d8453b32dbe982eea1fb8bbc4fc8e3bcccf2fc998f96138629","hash":"0a920b2e99a917a04d7961a28b49d05524d10cd8bdc2356c026cfc1c280ca22c"}
+                expect(res.status).to.equal(200);
+                let j = JSON.parse(res.text);
+                expect(j.success).to.equal(true);
+                expect(j.api_key).to.be.a('string');
+                expect(j.hash).to.be.a('string');
+                created_api_key_2 = j.api_key;
                 done();
             });
     }, 20000);
 
     // revoke
-    it("POST /api/user/apikey/revoke", function (done) {
+    it("POST /api/user/apikey/revoke (single)", function (done) {
         console.log("[chai] request /api/user/apikey/revoke (JWT)");
         chai.request(thx.app)
             .post('/api/user/apikey/revoke')
             .set('Authorization', jwt)
             .send({
-                'alias': 'mock-apikey-alias'
+                'fingerprint': created_api_key
             })
             .end((err, res) => {
                 console.log("[chai] response /api/user/apikey/revoke (JWT):", res.text, " status:", res.status);
-                //expect(res.status).to.equal(200);
+                // {"revoked":[],"success":true}
                 //expect(res.text).to.be.a('string');
+                let j = JSON.parse(res.text);
+                expect(j.success).to.equal(true);
+                expect(res.status).to.equal(200);
+                done();
+            });
+    }, 20000);
+
+    it("POST /api/user/apikey/revoke (multiple)", function (done) {
+        console.log("[chai] request /api/user/apikey/revoke (JWT)");
+        chai.request(thx.app)
+            .post('/api/user/apikey/revoke')
+            .set('Authorization', jwt)
+            .send({
+                'fingerprints': created_api_key_2
+            })
+            .end((err, res) => {
+                console.log("[chai] response /api/user/apikey/revoke (JWT):", res.text, " status:", res.status);
+                // {"revoked":[],"success":true}
+                //expect(res.text).to.be.a('string');
+                let j = JSON.parse(res.text);
+                expect(j.success).to.equal(true);
+                expect(res.status).to.equal(200);
+                // todo: expect(j.revoked.count > 0);
                 done();
             });
     }, 20000);
