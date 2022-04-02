5255a2c22c3ffda38464553aaaca14f8d92ea18e
Matej Sychra <suculent@me.com>
2022-04-02 16:25:12 +0200

    some spec fixes, trying to cover API keys with JWT as an example class where the implementation is split to agentful and agentless describe

--- spec/jasmine/ZZ-RouterAPIKeySpec.js
+++ spec/jasmine/ZZ-RouterAPIKeySpec.js
@@ -6,12 +6,15 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
-describe("API Keys", function () {
+//
+// Unauthenticated
+//
 
-    let thx;
+describe("API Keys (noauth)", function () {
+
+    let thx = new THiNX();
 
     beforeAll((done) => {
-        thx = new THiNX();
         thx.init(() => {
             done();
         });
@@ -61,5 +64,90 @@ describe("API Keys", function () {
                 done();
             });
     }, 20000);
+});
 
+//
+// Authenticated
+//
+
+
+describe("API Keys (JWT)", function () {
+
+    let thx = new THiNX();
+    let agent;
+    let jwt;
+
+    beforeAll((done) => {
+        thx.init(() => {
+            agent = chai.request.agent(thx.app);
+
+            agent
+                .post('/api/login')
+                .send({ username: 'dynamic', password: 'dynamic', remember: false })
+                .then(function (res) {
+                    console.log(`[chai] beforeAll POST /api/login (valid) response: ${JSON.stringify(res)}`);
+                    expect(res).to.have.cookie('x-thx-core');
+                    let body = JSON.parse(res.text);
+                    jwt = 'Bearer ' + body.access_token;
+                    done();
+                });
+        });
+    });
+
+    afterAll((done) => {
+        agent.close();
+        done();
+    });
+
+    //
+    // Unauthenticated
+    //
+
+    // create
+    it("POST /api/user/apikey (JWT)", function (done) {
+        console.log("[chai] request /api/user/apikey (JWT)");
+        chai.request(thx.app)
+            .post('/api/user/apikey')
+            .set('Authorization', jwt)
+            .send({
+                'alias': 'mock-apikey-alias'
+            })
+            .end((err, res) => {
+                console.log("[chai] response /api/user/apikey (JWT):", res.text, " status:", res.status);
+                //expect(res.status).to.equal(200);
+                //expect(res.text).to.be.a('string');
+                done();
+            });
+    }, 20000);
+
+    // revoke
+    it("POST /api/user/apikey/revoke (JWT)", function (done) {
+        console.log("[chai] request /api/user/apikey/revoke (JWT)");
+        chai.request(thx.app)
+            .post('/api/user/apikey/revoke')
+            .set('Authorization', jwt)
+            .send({
+                'alias': 'mock-apikey-alias'
+            })
+            .end((err, res) => {
+                console.log("[chai] response /api/user/apikey/revoke (JWT):", res.text, " status:", res.status);
+                //expect(res.status).to.equal(200);
+                //expect(res.text).to.be.a('string');
+                done();
+            });
+    }, 20000);
+
+    // list
+    it("GET /api/user/apikey/list (JWT)", function (done) {
+        console.log("[chai] request GET /api/user/apikey/list (JWT)");
+        chai.request(thx.app)
+            .get('/api/user/apikey/list')
+            .set('Authorization', jwt)
+            .end((err, res) => {
+                console.log("[chai] response /api/user/apikey/list (JWT):", res.text, " status:", res.status);
+                //expect(res.status).to.equal(200);
+                //expect(res.text).to.be.a('string');
+                done();
+            });
+    }, 20000);
 });
