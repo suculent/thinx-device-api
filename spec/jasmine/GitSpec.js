955a81e8fc92482ed0e7bec632704970012dfc94
Matej Sychra <suculent@me.com>
2022-04-05 15:45:25 +0200

    is there something fishy in GitSpec? seems good, but broke the buils

--- spec/jasmine/GitSpec.js
+++ spec/jasmine/GitSpec.js
@@ -7,65 +7,60 @@ describe("Git", function () {
     var device_path = deploy.pathForDevice(envi.oid, envi.udid);
     var dyn_device_path = deploy.pathForDevice(envi.dynamic.owner, envi.udid);
 
-    it("should be able to fetch one repo", function (done) {
+    it("should be able to fetch one repo", function () {
         let git = new Git();
         let success = git.fetch(
             "07cef9718edaad79b3974251bb5ef4aedca58703142e8c4c48c20f96cda4979c", // owner
             "git clone https://github.com/suculent/thinx-firmware-esp8266-pio", // command
             device_path
         );
-        expect(success).to.be.true;
-        done();
-    }, 10000);
+        expect(success === true);
+    });
 
-    it("should be able to fetch another repo", function (done) {
+    it("should be able to fetch another repo", function () {
         let git = new Git();
         let success = git.fetch(
             "07cef9718edaad79b3974251bb5ef4aedca58703142e8c4c48c20f96cda4979c", // owner
             "git clone https://github.com/suculent/thinx-firmware-esp32-pio", // command
             device_path
         );
-        expect(success).to.be.true;
-        done();
-    }, 10000);
+        expect(success === true);
+    });
 
-    it("should be able to fail safely", function (done) {
+    it("should be able to fail safely", function () {
         let git = new Git();
         let success = git.fetch(
             "07cef9718edaad79b3974251bb5ef4aedca58703142e8c4c48c20f96cda4979c", // owner
             "git clone https://github.com/suculent/doesnotexist", // command
             device_path
         );
-        expect(success).to.be.false;
-        done();
-    }, 10000);
+        expect(success === false);
+    });
 
     it("should survive invalid input", function () {
         let git = new Git();
         let success = git.tryShellOp(";", "&&");
-        expect(success).to.be.false;
+        expect(success === false);
     });
 
-    it("should be able to fetch first repo for dynamic owner", function (done) {
+    it("should be able to fetch first repo for dynamic owner", function () {
         let git = new Git();
         let success = git.fetch(
             "07cef9718edaad79b3974251bb5ef4aedca58703142e8c4c48c20f96cda4979c", // owner
             "git clone https://github.com/suculent/thinx-firmware-esp8266-pio", // command
             dyn_device_path
         );
-        expect(success).to.be.true;
-        done();
-    }, 10000);
+        expect(success === true);
+    });
 
-    it("should be able to fetch another repo for dynamic owner", function (done) {
+    it("should be able to fetch another repo for dynamic owner", function () {
         let git = new Git();
         let success = git.fetch(
             "07cef9718edaad79b3974251bb5ef4aedca58703142e8c4c48c20f96cda4979c", // owner
             "git clone https://github.com/suculent/thinx-firmware-esp32-pio", // command
             dyn_device_path
         );
-        expect(success).to.be.true;
-        done();
-    }, 10000);
+        expect(success === true);
+    });
 
 });
