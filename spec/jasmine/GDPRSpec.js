ffab3808d3cc9867ec717223866c1ae235481fcd
Matej Sychra <suculent@me.com>
2022-04-06 21:11:42 +0200

    proper GDPR fix

--- spec/jasmine/GDPRSpec.js
+++ spec/jasmine/GDPRSpec.js
@@ -1,29 +1,9 @@
-describe("GDPR", function() {
+describe("GDPR", function () {
 
     var envi = require("../_envi.json");
     var expect = require('chai').expect;
     let GDPR = require("../../lib/thinx/gdpr");
 
-    it("should not fail while scheduling guards", function() {
-        let gdpr = new GDPR();
-        expect(gdpr.guard()).to.equal(true);
-    }, 10000);
-
-    it("should not fail while purging", function(done) {
-        let gdpr = new GDPR();
-        gdpr.purgeOldUsers((result) => {
-            console.log("[spec] while purging", result);
-            done();
-        });
-    }, 10000);
-
-    it("should not fail while notifying", function(done) {
-        let gdpr = new GDPR();
-        gdpr.notifyOldUsers((result) => {
-            console.log("[spec] while notifying", result);
-            done();
-        });
-    }, 10000);
 
     let mock_user = {
         last_update: 1649088795,
@@ -32,7 +12,29 @@ describe("GDPR", function() {
         notifiedBeforeGDPRRemoval168: false
     };
 
-    it("should notify 24 hours before deletion", function(done) {
+    it("should not fail while scheduling guards", function () {
+        let gdpr = new GDPR();
+        expect(gdpr.guard()).to.equal(true);
+    }, 10000);
+
+    it("should not fail while purging", function (done) {
+        let gdpr = new GDPR();
+        gdpr.purgeOldUsers((result) => {
+            console.log("[spec] while purging", result);
+            done();
+        });
+    }, 10000);
+
+    it("should not fail while notifying", function (done) {
+        let gdpr = new GDPR();
+        gdpr.notifyOldUsers((result) => {
+            console.log("[spec] while notifying", result);
+            done();
+        });
+    }, 10000);
+
+
+    it("should notify 24 hours before deletion", function (done) {
         var d1 = new Date();
         d1.setMonth(d1.getMonth() - 3);
         d1.setDate(d1.getDay() - 1);
@@ -47,7 +49,7 @@ describe("GDPR", function() {
         });
     }, 10000);
 
-    it("should notify 3 months - 168 hours before deletion", function(done) {
+    it("should notify 3 months - 168 hours before deletion", function (done) {
         let gdpr = new GDPR();
         var d2 = new Date();
         d2.setMonth(d2.getMonth() - 4);
