ac1c51341fe72d24707edf63c3aef8212286a789
Matej Sychra <suculent@me.com>
2022-04-11 19:32:59 +0200

    on the contrary, seems to have a RC/TOCTOU

--- spec/jasmine/ZZ-AppSessionUserSpec.js
+++ spec/jasmine/ZZ-AppSessionUserSpec.js
@@ -300,7 +300,7 @@ describe("User Routes", function () {
         console.log("🚸 [chai] POST /api/user/password/set (4) response:", res.text, " status:", res.status);
         expect(res.status).to.equal(200);
         expect(res.text).to.be.a('string');
-        expect(res.text).to.equal('{"success":false,"status":"password_reset_successful"}');
+        expect(res.text).to.equal('{"success":false,"status":"password_reset_failed"}');
         done();
       });
   }, 20000);
