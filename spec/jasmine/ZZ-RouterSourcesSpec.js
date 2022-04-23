bf5b163e58431c3c1a8ab32beb16d9f2942f7c7a
Matej Sychra <suculent@me.com>
2022-04-23 18:44:54 +0200

    added v2 routes for sources; should finally fetch something

--- spec/jasmine/ZZ-RouterSourcesSpec.js
+++ spec/jasmine/ZZ-RouterSourcesSpec.js
@@ -105,6 +105,21 @@ describe("Sources (JWT)", function () {
             });
     }, 20000);
 
+
+    it("GET /api/v2/source", function (done) {
+        chai.request(thx.app)
+            .get('/api/v2/source')
+            .set('Authorization', jwt)
+            .end((err, res) => {
+                // {"success":true,"sources":{"7038e0500a8690a8bf70d8470f46365458798011e8f46ff012f12cbcf898b2f3":{"alias":"THiNX Vanilla ESP8266 Arduino","url":"https://github.com/********/*****-firmware-esp8266-ino.git","branch":"origin/master","platform":"arduino"},"7038e0500a8690a8bf70d8470f46365458798011e8f46ff012f12cbcf898b2f4":{"alias":"THiNX Vanilla ESP8266 Platform.io","url":"https://github.com/********/*****-firmware-esp8266-pio.git","branch":"origin/master","platform":"platformio"},"7038e0500a8690a8bf70d8470f46365458798011e8f46ff012f12cbcf898b2f5":{"alias":"THiNX Vanilla ESP8266 Lua","url":"https://github.com/********/*****-firmware-esp8266-lua.git","branch":"origin/master","platform":"nodemcu"},"7038e0500a8690a8bf70d8470f46365458798011e8f46ff012f12cbcf898b2f6":{"alias":"THiNX Vanilla ESP8266 Micropython","url":"https://github.com/********/*****-firmware-esp8266-upy.git","branch":"origin/master","platform":"micropython"},"7038e0500a8690a8bf70d8470f46365458798011e8f46ff012f12cbcf898b2f7":{"alias":"THiNX Vanilla ESP8266 MongooseOS","url":"https://github.com/********/*****-firmware-esp8266-mos.git","branch":"origin/master","platform":"mongoose"}}}
+                expect(res.status).to.equal(200);
+                let j = JSON.parse(res.text);
+                expect(j.success).to.equal(true);
+                expect(j.sources).to.be.an('object');
+                done();
+            });
+    }, 20000);
+
     it("POST /api/user/source (invalid)", function (done) {
         chai.request(thx.app)
             .post('/api/user/source')
@@ -120,6 +135,20 @@ describe("Sources (JWT)", function () {
     it("POST /api/user/source (semi-valid, does not fetch)", function (done) {
         chai.request(thx.app)
             .post('/api/user/source')
+            .set('Authorization', jwt)
+            .send(mock_source)
+            .end((err, res) => {
+                expect(res.text).to.be.a('string');
+                let r = JSON.parse(res.text);
+                expect(res.status).to.equal(200);
+                expect(r.success).to.equal(true);
+                done();
+            });
+    }, 20000);
+
+    it("PUT /api/v2/source (semi-valid, does not fetch)", function (done) {
+        chai.request(thx.app)
+            .put('/api/v2/source')
             .set('Authorization', jwt)
             .send(mock_source)
             .end((err, res) => {
@@ -152,6 +181,19 @@ describe("Sources (JWT)", function () {
             .end((err, res) => {
                 expect(res.status).to.equal(200);
                 expect(res.text).to.equal('{"success":true,"source_ids":[]}');
+                done();
+            });
+    }, 20000);
+
+    it("DELETE /api/user/source/revoke (valid)", function (done) {
+        chai.request(thx.app)
+            .delete('/api/v2/source')
+            .set('Authorization', jwt)
+            .send({ source_ids: source_for_revocation })
+            .end((err, res) => {
+                console.log("[chai] DELETE /api/v2/source response:", res.text, res.status);
+                expect(res.status).to.equal(200);
+                // expect(res.text).to.equal('{"success":true,"source_ids":[]}'); already deleted
                 done();
             });
     }, 20000);
