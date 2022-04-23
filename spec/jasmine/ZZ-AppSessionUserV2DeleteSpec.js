797aa1cd5f28b396cf9fdfc767aeb0c2765cd8f8
Matej Sychra <suculent@me.com>
2022-04-23 11:19:50 +0200

    smoke test for user v2 coverage (need dynamic2 owner id first to activate)

---
+++ spec/jasmine/ZZ-AppSessionUserV2DeleteSpec.js
@@ -0,0 +1,200 @@
+/* Router integration test only; does not have to cover full unit functionality. */
+
+const THiNX = require("../../thinx-core.js");
+
+let chai = require('chai');
+var expect = require('chai').expect;
+let chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+var envi = require("../_envi.json");
+
+const dynamic_owner_id = envi.dynamic2.owner;
+
+const user_info = {
+  first_name: "Dynamic",
+  last_name: "User",
+  email: "dynamic2@example.com",
+  username: "dynamic2"
+};
+
+let dynamic_activation_code = null;
+
+let thx;
+let agent;
+let jwt = null;
+
+let reset_key = null;
+
+describe("User Routes V2", function () {
+
+  beforeAll((done) => {
+    thx = new THiNX();
+    thx.init(() => {
+      agent = chai.request.agent(thx.app);
+      console.log(`🚸 [chai] >>> running User Routes spec`);
+      done();
+    });
+  });
+
+  afterAll((done) => {
+    agent.close();
+    console.log(`🚸 [chai] <<< completed User Routes spec`);
+    done();
+  });
+
+  //
+  // User Lifecycle
+  //
+
+  it("POST /api/v2/user/create (valid body) and activate (set password)", function (done) {
+    chai.request(thx.app)
+      .post('/api/v2/user')
+      .send(user_info)
+      .end((_err, res) => {
+        // {"success":true,"status":"6975d3c5849fc130e689f2cae0abe51a8fd24f496810bee3c0bcf531dd53be0c"}
+        expect(res.text).to.be.a('string');
+        expect(res.status).to.equal(200);
+        let body = JSON.parse(res.text);
+        dynamic_activation_code = body.status;
+        expect(body.status).to.be.a('string'); // check length
+        expect(body.status.length == 64);
+
+        let rurl = '/api/v2/user/activate?owner=' + dynamic_owner_id + '&activation=' + dynamic_activation_code;
+        chai.request(thx.app)
+          .get(rurl)
+          .end((__err, __res) => {
+            expect(__res.status).to.equal(200);
+            expect(__res.text).to.be.a('string'); // <html>
+            expect(__res).to.be.html;
+
+            chai.request(thx.app)
+              .post('/api/v2/password/set')
+              .send({ password: envi.dynamic.username, rpassword: envi.dynamic.username, activation: dynamic_activation_code })
+              .end((___err, ___res) => {
+                expect(___res.status).to.equal(200);
+                expect(___res.text).to.be.a('string');
+                expect(___res.text).to.equal('{"success":true,"status":"password_reset_successful"}');
+                done();
+              });
+          });
+
+      });
+  }, 20000);
+
+  it("POST /api/v2/password/set", function (done) {
+    chai.request(thx.app)
+      .post('/api/v2/password/set')
+      .send({ password: "dynamic", rpassword: "dynamic", reset_key: reset_key })
+      .end((_err, res) => {
+        console.log("🚸 [chai] POST /api/user/password/set (valid) X response:", res.text, " status:", res.status);
+        expect(res.status).to.equal(200);
+        expect(res.text).to.be.a('string');
+        //expect(res.text).to.equal('{"success":false,"status":"password_reset_failed"}'); // somehow not deterministic
+        done();
+      });
+  }, 20000);
+
+  it("GET /api/v2/password/reset", function (done) {
+    chai.request(thx.app)
+      .get('/api/v2/password/reset')
+      .end((_err, res) => {
+        console.log("🚸 [chai] GET /api/v2/password/reset response:", res.text, " status:", res.status);
+        expect(res.status).to.equal(200);
+        expect(res.text).to.be.a('string');
+        //expect(res.text).to.equal('{"success":false,"status":"password_reset_failed"}'); // somehow not deterministic
+        done();
+      });
+  }, 20000);
+
+  it("POST /api/v2/password/reset", function (done) {
+    chai.request(thx.app)
+      .post('/api/v2/password/reset')
+      .send({ password: "dynamic", rpassword: "dynamic", reset_key: reset_key })
+      .end((_err, res) => {
+        console.log("🚸 [chai] POST /api/v2/password/reset response:", res.text, " status:", res.status);
+        expect(res.status).to.equal(200);
+        expect(res.text).to.be.a('string');
+        //expect(res.text).to.equal('{"success":false,"status":"password_reset_failed"}'); // somehow not deterministic
+        done();
+      });
+  }, 20000);
+
+  //
+  // User Profile
+  //
+
+  it("POST /api/login + /api/v2/gdpr", function (done) {
+    agent
+      .post('/api/login')
+      .send({ username: 'dynamic2', password: 'dynamic', remember: false })
+      .then(function (res) {
+        expect(res).to.have.cookie('x-thx-core');
+        /* response example:
+        {
+          "status":"OK",
+          "success":true,
+          "access_token":"eyJh...",
+          "redirectURL":"https://rtm.thinx.cloud/auth.html?t=33ed0c670113f6e8b1095a1b1857d5dc6e9db77c37122d76c45ffacef2484701&g=true"
+        } */
+        let body = JSON.parse(res.text);
+        jwt = 'Bearer ' + body.access_token;
+
+        // Old UI does this
+        let token = body.redirectURL.replace("https://rtm.thinx.cloud/auth.html?t=", "").replace("&g=true", "");
+
+        // just test-added
+        agent
+          .post('/api/v2/gdpr')
+          .send({ gdpr: true, token: token })
+          .end((_err, _res) => {
+            console.log("[chai] POST /api/gdpr response:", _res.text, "status", _res.status);
+            expect(_res.status).to.equal(200);
+            expect(_res.text).to.be.a('string');
+            // {"success":false,"status":"invalid_protocol_update_key_missing"} // WTF?
+
+            return agent
+              .post('/api/login')
+              .send({ token: token })
+              .end((_err1, res1) => {
+                expect(res1.status).to.equal(200);
+                done();
+              });
+          });
+      })
+      .catch((e) => { console.log(e); });
+  }, 20000);
+
+  //
+  // User Statistics
+  //
+
+  it("GET /api/v2/stats", function (done) {
+    chai.request(thx.app)
+      .get('/api/v2/stats')
+      .set('Authorization', jwt)
+      .end((_err, res) => {
+        expect(res.status).to.equal(401);
+        //expect(res.text).to.be.a('string');
+        done();
+      });
+  }, 20000);
+
+  it("POST /api/v2/chat", function (done) {
+    chai.request(thx.app)
+      .post('/api/v2/chat')
+      .set('Authorization', jwt)
+      .send({})
+      .end((_err, res) => {
+        console.log("🚸 [chai] POST /api/v2/chat response:", res.text, " status:", res.status);
+        expect(res.status).to.equal(200);
+        //expect(res.text).to.be.a('string');
+        done();
+      });
+  }, 20000);
+
+  //
+  // Removal
+  //
+
+});
