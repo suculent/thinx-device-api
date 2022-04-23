c923529895180255e7ac2023beebad15df46704b
Matej Sychra <suculent@me.com>
2022-04-23 19:49:40 +0200

    spec fixes and cleanup

--- spec/jasmine/ZZ-RouterDeviceAPISpec.js
+++ spec/jasmine/ZZ-RouterDeviceAPISpec.js
@@ -63,7 +63,7 @@ describe("Device API (noauth)", function () {
             .send({ push: "31b1f6bf498d7cec463ff2588aca59a52df6f880e60e8d4d6bcda0d8e6e87823" })
             .end((err, res) => {
                 console.log("[chai] device add Push registration", res.text, res.status);
-                expect(res.status).to.equal(404);
+                expect(res.status).to.equal(403);
                 //expect(res.text).to.be.a('string');
                 done();
             });
@@ -222,11 +222,24 @@ describe("Device + API (JWT+Key)", function () {
 
     it("POST /device/addpush (jwt, invalid)", function (done) {
         chai.request(thx.app)
-            .post('/api/device/addpush')
+            .post('/device/addpush')
             .set('Authentication', ak)
             .send({})
             .end((err, res) => {
-                expect(res.status).to.equal(404);
+                expect(res.status).to.equal(403);
+                //expect(res.text).to.be.a('string');
+                done();
+            });
+    }, 20000);
+
+    it("POST /device/addpush (jwt, valid)", function (done) {
+        chai.request(thx.app)
+            .post('/device/addpush')
+            .set('Authentication', ak)
+            .send({ push: "31b1f6bf498d7cec463ff2588aca59a52df6f880e60e8d4d6bcda0d8e6e87823", udid: envi.udid })
+            .end((err, res) => {
+                console.log("[chai] POST /device/addpush (jwt, valid) result", res.text);
+                //expect(res.status).to.equal(403);
                 //expect(res.text).to.be.a('string');
                 done();
             });
