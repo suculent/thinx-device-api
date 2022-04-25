f157075f5cd75985b15dbb9003d8fcb88198e758
Matej Sychra <suculent@me.com>
2022-04-25 20:24:05 +0200

    potential fix in atomic user update that might affect password set event

--- spec/jasmine/ZZ-AppSessionUserV2DeleteSpec.js
+++ spec/jasmine/ZZ-AppSessionUserV2DeleteSpec.js
@@ -90,7 +90,7 @@ describe("User Routes V2", function () {
       .post('/api/v2/password/reset')
       .send({ email: envi.dynamic2.email })
       .end((_err, res) => {
-        console.log("🚸 [chai] V2 POST /api/v2/password/reset (1) response:", res.text, " status:", res.status);
+        console.log("🚸 [chai] V2 POST /api/v2/password/reset (1) response:", res.text, " status:", res.status); // true!
         let j = JSON.parse(res.text);
         reset_key = j.status;
         expect(res.status).to.equal(200);
@@ -104,7 +104,7 @@ describe("User Routes V2", function () {
     chai.request(thx.app)
       .get('/api/v2/password/reset?owner_id='+envi.dynamic2.owner+'&reset_key='+reset_key)
       .end((_err, res) => {
-        //console.log("🚸 [chai] V2 GET /api/v2/password/reset (2) response:", res.text, " status:", res.status); is long HTML
+        console.log("🚸 [chai] V2 GET /api/v2/password/reset (2) response:", res.text, " status:", res.status); is long HTML
         expect(res.status).to.equal(200);
         expect(res.text).to.be.a('string');
         //expect(res.text).to.equal('{"success":false,"status":"password_reset_failed"}'); // somehow not deterministic
