af38ca2098a4c8bd6001388134882367faa91a5b
Matej Sychra <suculent@me.com>
2022-04-26 19:02:27 +0200

    returning success with findNext to prevent ambiguity

--- spec/jasmine/QueueSpec.js
+++ spec/jasmine/QueueSpec.js
@@ -42,7 +42,8 @@ describe("Queue", function () {
         queue_with_cron.add(mock_udid_1, mock_source_id, mock_owner_id, () => {
             queue_with_cron.add(mock_udid_2, mock_source_id, mock_owner_id, () => {
                 queue_with_cron.add(mock_udid_3, mock_source_id, mock_owner_id, () => {
-                    queue_with_cron.findNext((next) => {
+                    
+                    queue_with_cron.findNext((success, next) => {
 
                         if (next === null) {
                             if (done_called === false) {
@@ -56,7 +57,7 @@ describe("Queue", function () {
                         queue_with_cron.runNext(next, workers[0]);
 
                         // Should not be able to find anything while queue item is running
-                        queue_with_cron.findNext((/* nextAction */) => {
+                        queue_with_cron.findNext((success, next) => {
 
                             if (next === null) {
                                 if (done_called === false) {
