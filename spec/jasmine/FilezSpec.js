aede4dc78c819acf3b604e65e1a4a7c350114fe5
Matej Sychra <suculent@me.com>
2022-05-08 17:47:16 +0200

    new spec fix

--- spec/jasmine/FilezSpec.js
+++ spec/jasmine/FilezSpec.js
@@ -8,23 +8,23 @@ var envi = require("../_envi.json");
 
 describe("Filez", function () {
 
-    beforeAll(() => {
-        console.log(`🚸 [chai] >>> running Filez spec`);
-      });
-    
-      afterAll(() => {
-        console.log(`🚸 [chai] <<< completed Filez spec`);
-      });
+  beforeAll(() => {
+    console.log(`🚸 [chai] >>> running Filez spec`);
+  });
 
-      it("should provide app's default root", function () {
-        let result = Filez.appRoot();
-        expect(result).to.be.a('string');
-        expect(result).to.equal('/opt/thinx/thinx-device-api');
-    });
-    
-    it("should provide deploy path for owner's device", function () {
-        let result = Filez.deployPathForDevice(envi.owner, wnvi.udid);
-        expect(result).to.be.a('string');
-    });
-    
+  afterAll(() => {
+    console.log(`🚸 [chai] <<< completed Filez spec`);
+  });
+
+  it("should provide app's default root", function () {
+    let result = Filez.appRoot();
+    expect(result).to.be.a('string');
+    expect(result).to.equal('/opt/thinx/thinx-device-api');
+  });
+
+  it("should provide deploy path for owner's device", function () {
+    let result = Filez.deployPathForDevice(envi.owner, envi.udid);
+    expect(result).to.be.a('string');
+  });
+
 });
