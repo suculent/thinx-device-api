146ef32ddb558f6557db570550d2c156b7f7b28c
Matej Sychra <suculent@me.com>
2023-01-27 23:37:49 +0100

    with 5 failing specs; queue refactored to async to solve complex semantics; mesh attach seems to get wrong error object

--- spec/jasmine/QueueSpec.js
+++ spec/jasmine/QueueSpec.js
@@ -30,7 +30,7 @@ describe("Queue", function () {
     let queue_with_cron;
 
     // init
-    it("should not fail or hang", function (done) {
+    it("should not fail or hang", async (done) => {
 
         let builder = new Builder(redis);
 
@@ -49,38 +49,35 @@ describe("Queue", function () {
         // Should be able to add actions to the queue
         queue_with_cron.add(mock_udid_1, mock_source_id, mock_owner_id, () => {
             queue_with_cron.add(mock_udid_2, mock_source_id, mock_owner_id, () => {
-                queue_with_cron.add(mock_udid_3, mock_source_id, mock_owner_id, () => {
+                queue_with_cron.add(mock_udid_3, mock_source_id, mock_owner_id, async () => {
 
-                    queue_with_cron.findNext((success, next) => {
+                    let next = await queue_with_cron.findNext();
 
-                        if ((next === null) || (success === false)) {
-                            if (done_called === false) {
-                                done_called = true;
-                                done();
-                            }
-                            return;
+                    if ((next === null) || (success === false)) {
+                        if (done_called === false) {
+                            done_called = true;
+                            done();
                         }
+                        return;
+                    }
 
-                        // Should be able run next item
-                        queue_with_cron.runNext(next, workers[0]);
+                    // Should be able run next item
+                    queue_with_cron.runNext(next, workers[0]);
 
-                        // Should not be able to find anything while queue item is running
-                        queue_with_cron.findNext((success, next) => {
+                    // Should not be able to find anything while queue item is running
+                    next = await queue_with_cron.findNext();
 
-                            if (next === null) {
-                                if (done_called === false) {
-                                    done_called = true;
-                                    done();
-                                }
-                                return;
-                            }
+                    if (next === null) {
+                        if (done_called === false) {
+                            done_called = true;
+                            done();
+                        }
+                        return;
+                    }
 
-                            // Should run loop safely
-                            for (let i = 0; i < 10; i++) {
-                                queue_with_cron.loop();
-                            }
-                        });
-                    });
+                    // Should run loop safely
+                    for (let i = 0; i < 10; i++) queue_with_cron.loop();
+
                 });
             });
         });
