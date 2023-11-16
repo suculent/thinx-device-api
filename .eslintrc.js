0d6887e1f38754712452af91a278aa6dadf22a37
Matej Sychra <suculent@me.com>
2023-11-16 18:12:41 +0100

    next step

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+{
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "script"
+    },
+    "rules": {
+        "indent": 0,
+        "linebreak-style": 0,
+        "semi": 0,
+        "no-console": 0,
+        "lines-around-comment": 0,
+        "max-len": 0,
+        "camelcase": 0,
+        "eqeqeq": 0,
+        "dot-notation": 0,
+        "no-useless-escape": 0,
+        "no-undef": 0,
+        "space-in-parens": 0,
+        "quotes": 0,
+        "key-spacing": 0
+    }
+}
